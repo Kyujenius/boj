0eef02e5e662a114bc52358b885a9c51033d1b19
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-04-11 22:11:58 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,42 +1,42 @@
 function solution(n, w, num) {
     // 22 개의 박스를 , 6의 width로 쌓았을 때, 8번째 박스를 꺼내려면 드는 횟수 구하기
     var answer = 0;
-    let numHeight = Math.ceil(num / w );
-    let numPosition = num % w ;
-    let ceilHeight = Math.ceil(n / w );
-    let ceilPosition = n % w ;
+    let numHeight = Math.ceil(num / w ); // 2
+    let numPosition = num % w ; // 2
+    let ceilHeight = Math.ceil(n / w ); // 4
+    let ceilPosition = n % w ; // 4
     
     if(numPosition == 0) {
-        numPosition = w;
+        numPosition = w; //6의 배수면 6으로
     }
     
     if(ceilPosition == 0) {
-        ceilPosition = w;
+        ceilPosition = w; //6의 배수면 6으로
     }
     //짝수번째면 position의 위치를 바꿔버려
     
     if(numHeight %2 == 0  && ceilHeight %2 ==0) {
-        if(ceilPosition >= numPosition) {
-            answer = ceilHeight - numHeight +1;
-        }else {
-            answer = ceilHeight - numHeight;
-        }
+        and();
         
     }else if(numHeight %2 !== 0  && ceilHeight %2 !==0) {
+        and();
+    }
+    else if(numHeight %2 == 0  && ceilHeight %2 !==0) {
+        xor();
+    }
+    else if(numHeight %2 !== 0  && ceilHeight %2 ==0) {
+        xor();
+    }
+    
+    function and() {
         if(ceilPosition >= numPosition) {
             answer = ceilHeight - numHeight +1;
         }else {
             answer = ceilHeight - numHeight;
         }
     }
-    else if(numHeight %2 == 0  && ceilHeight %2 !==0) {
-        if(ceilPosition + numPosition > w) {
-            answer = ceilHeight - numHeight +1;
-        }else {
-            answer = ceilHeight - numHeight;
-        }
-    }
-    else if(numHeight %2 !== 0  && ceilHeight %2 ==0) {
+    
+    function xor() {
         if(ceilPosition + numPosition > w) {
             answer = ceilHeight - numHeight +1;
         }else {
