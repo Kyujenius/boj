92ff1bab303a46dc5ea7f6d1ca868b149f86a278
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-04-12 16:41:58 +0900

    [level 0] Title: 배열 조각하기, Time: 2.17 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -1,18 +1,13 @@
 function solution(arr, query) {
-    let changingArr = arr.slice(); // 원본 배열 복사
-    
-    for(let i = 0; i < query.length; i++) {
-        const value = query[i];
-        
-        // 짝수 인덱스일 때 (0부터 시작하므로 i % 2 == 0)
-        if(i % 2 === 0) {
-            // 뒷부분 자르기: 0부터 value까지 유지
-            changingArr = changingArr.slice(0, value + 1);
-        } else {
-            // 앞부분 자르기: value부터 끝까지 유지
-            changingArr = changingArr.slice(value);
+    let newArr = arr;
+    for(let i =0 ; i<query.length; i++) {
+        const queryNumber = query[i];
+        if(i %2 == 0) {
+            newArr  = newArr.slice(0,queryNumber+1);
+        }else {
+            newArr  = newArr.slice(queryNumber, newArr.length);
         }
     }
-    
-    return changingArr;
+
+    return newArr;
 }
