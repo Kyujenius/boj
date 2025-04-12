11a8a6abbed6dcdd7a3f35e3146de3c8ea2d38bf
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-04-12 18:16:44 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 3.65 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -6,19 +6,9 @@ function solution(arr) {
 
     console.log(`width: ${width}, height: ${height}, N : ${N}`)
     
-    for(let i= 0; i<N; i++) {
-        for(let k =0; k<N; k++) {
-            try{
-            if(arr[i][k] !==undefined) {
-                answer[i][k] = arr[i][k];
-            }else {
-                answer[i][k] = 0
-            }
-                }
-            catch {
-                                answer[i][k] = 0
-
-            }
+    for(let i= 0; i<height; i++) {
+        for(let k =0; k<width; k++) {
+            answer[i][k] = arr[i][k];
         }
     }
     
