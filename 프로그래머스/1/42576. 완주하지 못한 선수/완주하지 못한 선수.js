389b8380b8e3d09adf0d4af764bba78346b8e302
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-09-18 18:14:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.34 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,21 +1,22 @@
 function solution(participant, completion) {
+    
     var answer = '';
-    let object = {};
-    participant.forEach((value) => {
-        if(!object[value]) {
-            object[value] = 1;
-        }else {
-            object[value] += 1;
-        }
-    })
-    completion.forEach((value) => {
-        object[value] -= 1;
-    })
-    const entries = Object.entries(object).filter(([key,value])=> {
-        return value >=1;
-    })
-    answer = entries[0][0];
+    let result = {};
+    participant.forEach((value, index) => {
+      if (result[value]) {
+        result[value] += 1;
+      } else {
+        result[value] = 1;
+      }
+    });
     
+    completion.forEach((value,index) => {
+        result[value] -= 1;
+    })
+
+
     
-    return answer;
+    for(let key in result) {
+     if(result[key] == 1) return key;   
+    }
 }
