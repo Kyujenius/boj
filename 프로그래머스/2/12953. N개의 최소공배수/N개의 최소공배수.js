b5b242bde1ae138437d1eff6792d371e7e4865c1
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-10-02 10:11:37 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,19 +1,18 @@
 function solution(arr) {
     var answer = 0;
-    //모든 약수들이 들어가있고, 그 약수들 중 안 겹치게 곱하기만 하면 됨.
-    //둘이 곱한 걸 최대공약수로 나누면, 최소공배수 => 
-    return arr.reduce((acc, cur) => lcm(acc, cur));
+    answer = arr.reduce((acc,curr,index,array)=> 
+        (acc = lcm(acc,curr)))
+    return answer;
 }
 
-function gcd(a,b) {
-    // console.log("gcd : " , a,b)
+function gcp(a,b) {
     if(b === 0) {
         return a;
-    }else{
-        return gcd(b, a % b);        
+    }else {
+        return gcp(b, a % b);
     }
 }
 
 function lcm(a,b) {
-    return (a*b) / gcd(a,b);
+    return (a*b)/ gcp( a , b );
 }
