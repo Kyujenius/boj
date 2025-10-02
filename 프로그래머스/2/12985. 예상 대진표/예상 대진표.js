d994dd528ba08885dba7bc9d84d4b7767152a6cd
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-10-02 22:54:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,16 +1,9 @@
-function solution(n, a, b) {
-    let answer = 0; // 라운드를 저장할 변수
+function solution(n,a,b)
+{
+    var answer = 0;
 
-    // a와 b의 번호가 같아질 때까지 루프를 반복합니다.
-    // 같아진다는 것은 같은 경기로 배정되었다는 의미입니다.
-    while (a !== b) {
-        // 다음 라운드로 진출하므로 라운드를 1 증가시킵니다.
-        answer++;
-        
-        // a와 b를 각각 다음 라운드의 번호로 업데이트합니다.
-        a = Math.ceil(a / 2);
-        b = Math.ceil(b / 2);
-    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
 
     return answer;
 }
