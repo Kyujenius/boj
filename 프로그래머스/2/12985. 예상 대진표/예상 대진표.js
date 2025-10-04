07196df62ee668f30bf0a5c57adc53ae9fcf62da
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-10-04 12:42:26 +0900

    [level 2] Title: 예상 대진표, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,9 +1,12 @@
 function solution(n,a,b)
 {
-    var answer = 0;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-
-    return answer;
+/**
+*/
+    let count = 0;
+    while(a!==b) {
+        a= Math.ceil(a/2);
+        b= Math.ceil(b/2);
+        count++
+    }
+    return count;
 }
