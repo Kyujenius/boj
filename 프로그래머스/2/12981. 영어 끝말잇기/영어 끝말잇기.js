78278fda4e6d66b92133c5a5c8d15567b3721cca
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-10-04 12:30:16 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 32.7 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,26 +1,21 @@
 function solution(n, words) {
-    const doneWord = new Set();
-    
-    doneWord.add(words[0]);
+    const wordSet = new Set();
+    let beforeWord = words[0];
+    wordSet.add(beforeWord);
     for(let i = 1; i<words.length; i++) {
-        const afterWord = words[i];
-        const beforeWord = words[i-1];
-
-        const personNum = (i % n) +1;
-        const wordNum = Math.floor(i / n)+1;
-        // 글자가 다를 때, 
-        if(beforeWord[beforeWord.length-1] !== afterWord[0]) {
-            console.log("달라요");
-            return [personNum, wordNum]
+        let currentWord = words[i];
+        const personNumber = (i % n) +1;
+        const countNumber = Math.floor(i / n)+1;
+        //같은 첫 글자와 마지막 글자가 다른 경우
+        if(beforeWord[beforeWord.length-1] !== currentWord[0]) {
+            return [personNumber, countNumber];
+        }else if(wordSet.has(currentWord)) {
+            return [personNumber, countNumber];
+        } else {
+            beforeWord = currentWord;
+            wordSet.add(currentWord);
         }
-        // 글자를 이미 갖고 있을 때,
-        if(doneWord.has(afterWord)) {
-            console.log("있어요");
-            return [personNum, wordNum]
-        }
-        doneWord.add(afterWord);
-        
     }
-    return [0,0]
     
+    return [0,0];
 }
