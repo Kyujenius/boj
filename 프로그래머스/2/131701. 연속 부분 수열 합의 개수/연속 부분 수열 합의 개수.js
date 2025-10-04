f29503468040c17222d2c18cb46f7b2934fab6a7
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-10-04 12:50:36 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 155.04 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,11 +1,11 @@
 function solution(elements) {
-    const addSet = new Set();
-    for(let i = 0; i<elements.length; i++) {
-        let addAll = 0;
-        for(let j=0; j<elements.length; j++) {
-            addAll += elements[(i+j)%elements.length];
-            addSet.add(addAll)
+    const numberSet = new Set();
+    for(let i = 0 ; i<elements.length; i++) {
+        let startNumber = 0;
+        for(let j=0;j<elements.length; j++) {
+            startNumber += elements[(i+j) % elements.length];
+            numberSet.add(startNumber);
         }
     }
-    return addSet.size;
+    return numberSet.size;
 }
