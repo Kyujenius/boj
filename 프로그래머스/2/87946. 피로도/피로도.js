291dbbb4671b32afa0e014edcda2201a5f836e4e
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-10-09 15:04:22 +0900

    [level 2] Title: 피로도, Time: 5.10 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,17 +1,18 @@
 function solution(k, dungeons) {
-    let numDungeons = dungeons.length;
-    let visited = Array(numDungeons).fill(false);
-    let maxNum = 0;
+    const numDungeons = dungeons.length;
+    const visited = Array(numDungeons).fill(false);
+    let maxCount = 0;
     function dfs(currentK, count) {
-        maxNum = Math.max(maxNum,count);
-        for(let i = 0; i<numDungeons; i++) {
-            if(currentK >= dungeons[i][0] && visited[i] == false) {
-                visited[i] = true;      
-                dfs(currentK - dungeons[i][1], count+1);
-                visited[i] = false;
-            }
-        }
+        maxCount = Math.max(count, maxCount);
+        for(let i = 0 ; i <numDungeons; i++) {
+                if(visited[i] == false && currentK >= dungeons[i][0]) {
+                    visited[i] = true;
+                    dfs(currentK-dungeons[i][1], count+1);
+                    visited[i] = false;
+                }
+            }    
     }
-    dfs(k, 0)
-    return maxNum;
+    dfs(k,0)
+    
+    return maxCount;
 }
