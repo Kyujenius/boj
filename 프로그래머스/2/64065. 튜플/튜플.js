a3965fa3dee94fbb3ac36370047b8c1addd6d9e9
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-10-10 14:30:20 +0900

    [level 2] Title: 튜플, Time: 22.27 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,19 +1,18 @@
 function solution(s) {
     var answer = [];
-    const slicedS = s.slice(1,s.length-1);
-    const doubleSlicedS = slicedS.slice(1,slicedS.length-1);
-    const array = doubleSlicedS.split(`},{`);
-    const newNumberSet = new Set();
-    array.sort((a,b)=> a.length- b.length );
-    // console.log(array);
-    array.forEach((value) => {
-        const numArray = value.split(",");
-        numArray.forEach((number)=> {
-            newNumberSet.add(number);
+    const slicedS = s.slice(2, s.length-2);
+    const slicedSArray = slicedS.split(`},{`);
+    const tupleSet = new Set();
+    slicedSArray.sort((a,b)=>a.length-b.length);
+    console.log(slicedSArray);    
+        
+    slicedSArray.forEach((value)=> {
+        const array = value.split(',');
+        array.forEach((number)=> {
+            tupleSet.add(number);
         })
     })
-    answer = [...newNumberSet].map((value)=> parseInt(value,10));
-    // console.log(array);
-    // console.log(newNumberSet);
+    answer = [...tupleSet].map((value)=> parseInt(value,10));
+
     return answer;
 }
