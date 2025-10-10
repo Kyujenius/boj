ebe834e7149984882482373088c442047e531bf5
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-10-10 14:25:14 +0900

    [level 2] Title: [1차] 캐시, Time: 86.12 ms, Memory: 46.3 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,41 +1,33 @@
 function solution(cacheSize, cities) {
     var answer = 0;
-    const cacheArray = [];
-        // 이 부분을 추가!
+    const lruCache = [];
+    const lowerCaseCities = cities.map((value)=> value.toLowerCase())
     if (cacheSize === 0) {
-        return cities.length * 5;
-    }
+    return cities.length * 5;
+}
 
-    //LRU Cache를 구현해야하는데, 
-    function cachePush(cacheArray, inputValue, cacheSize) {
-        //있으면 맨 뒤로 보내야함.
-        // console.log(cacheArray, inputValue, cacheSize);
-        if(cacheArray.some((value) => value == inputValue)) {
-            const index = cacheArray.indexOf(inputValue)
-            cacheArray.splice(index, 1);
-            cacheArray.push(inputValue);
+    //LRU 캐시를 적용한 push 시스템
+    function cachePush(lruCache, inputValue) {
+        if(lruCache.some((cacheMemoryValue)=> inputValue == cacheMemoryValue)) {
+            const index = lruCache.indexOf(inputValue);
+            lruCache.splice(index,1);
+            lruCache.push(inputValue);
         }else {
-            if(cacheArray.length === cacheSize) {
-                cacheArray.shift();
+            if(lruCache.length === cacheSize) {
+                lruCache.shift();
             }
-            //없으면 추가해야함
-            cacheArray.push(inputValue);
+            lruCache.push(inputValue);
         }
     }
-    const lowerCity = cities.map((value)=> {
-        return value.toLowerCase();
-    })
-    lowerCity.forEach((city,index,array)=> {
-        //하나라도 있으면 answer +1
-        if(cacheArray.some((value) => value == city)) {
-            answer +=1;
-            // console.log(`있어요 value: ${city}`)
-        }else {
-        //하나도 없으면 answer +5
-            answer +=5;
-        }
-        cachePush(cacheArray, city, cacheSize);
-    })
 
+    lowerCaseCities.forEach((value)=> {
+        if(lruCache.some((cacheMemoryValue)=> value == cacheMemoryValue)) {
+            answer += 1;
+        }else {
+            answer += 5;
+        }
+        cachePush(lruCache, value);
+    })
+    
     return answer;
 }
