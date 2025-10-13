f19b3e92471c19b78fb11f85b935b0e7ca0ffb4c
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-10-13 15:09:08 +0900

    [level 2] Title: 타겟 넘버, Time: 8.84 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,22 @@
 function solution(numbers, target) {
-    var answer = 0;
-    function dfs(index, sum) {
-        if(index == numbers.length) {
-            if(sum === target) {
-                answer++;
-            }
-            return;
+    let answer =0 ;
+    
+    function dfs(acc, index) {
+        const plus = acc+numbers[index];
+        const minus = acc-numbers[index];
+        if(index == numbers.length-1) {
+             if(plus == target) {
+                answer++;                 
+             }else if (minus == target){
+                 answer++;
+             }
+        }else {
+            dfs(plus, index+1);
+            dfs(minus, index+1);
         }
-        const plus = sum + numbers[index];
-        const minus = sum - numbers[index];
-        
-        dfs(index+1, plus);
-        dfs(index+1, minus);
     }
     
     dfs(0,0);
+
     return answer;
 }
