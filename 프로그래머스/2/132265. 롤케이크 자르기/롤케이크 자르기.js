615f486d5b2772cab07ee24f9d31ad1ff84f649a
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-10-18 11:51:25 +0900

    [level 2] Title: 롤케이크 자르기, Time: 137.00 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,23 +1,22 @@
 function solution(topping) {
-    let answer =0;
+    var answer = 0;
     const aMap = new Map();
     const bMap = new Map();
-    topping.forEach((value) => aMap.set(value, (aMap.get(value) || 0) +1))
-    console.log(aMap,bMap);
-    for(let i=0; i<topping.length; i++) {
-        const value = topping[i]
+    topping.forEach((value) => aMap.set(value, (aMap.get(value) || 0) + 1));
+    console.log(aMap);
+    for(let i =0 ; i< topping.length; i++) {
+        const value = topping[i];
+        bMap.set(value, (bMap.get(value) || 1) + 1);
         if(aMap.get(value) == 1) {
             aMap.delete(value);
-        }else {
-            aMap.set(value, aMap.get(value)-1);
+        } else {
+            aMap.set(value, aMap.get(value) - 1);
         }
-        
-        bMap.set(value, (bMap.get(value) || 1) +1);
-        if(aMap.size === bMap.size) {
-            // console.log(aMap,bMap);
+        // console.log(aMap,bMap);
+        if(bMap.size == aMap.size) {
             answer++;
         }
     }
-    return answer;
     
+    return answer;
 }
