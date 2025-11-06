7deace757c13895a88cdb3a1ff4ce1ed0580c30a
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-11-06 23:26:48 +0900

    [level 3] Title: 네트워크, Time: 0.46 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,20 +1,35 @@
 function solution(n, computers) {
     var answer = 0;
-    let visited = Array(n).fill(false);
-    function dfs(node) {
-        visited[node] = true;
-        computers[node].forEach((value,index)=> {
-            if(value === 1 && !visited[index]) {
-                dfs(index);
-            }
-        })
-    }
+    //n 번까지 하나씩 찾으면서, 각 node랑 연결된 모든 것들을 visited로 취급함.
+    //다만, 한 번 노드의 시작할 때만, count+1 하기
+    const length = computers.length;
+    const visited = Array(length).fill(false);
+    const queue= [];
     
-    for(let i = 0 ; i <n; i++) {
-        if(!visited[i]) {
-            dfs(i);
-            answer++;
+    
+    function dfs(i) {
+        answer++;
+        visited[i] = true;
+        const node = computers[i];
+        queue.push(node);
+        while(queue.length >0) {
+            const shiftedNode = queue.shift();
+            for(let j = 0 ; j<shiftedNode.length ; j++) {
+                //연결됐고, 내가 아니고, 방문했던 곳이 아니면 queue에 추가, 
+                if(j !== i && shiftedNode[j] == 1 && visited[j] == false) {
+                    visited[j] = true;
+                    queue.push(computers[j]);
+                }
+            }
         }
     }
+    for(let i =0 ; i<length; i++) {
+        if(visited[i] == false) {
+            // 최초 방문 안 한 곳이면, dfs로 계속 node의 끝까지 탐색해야함
+            dfs(i);
+        }
+    }
+    
+    
     return answer;
 }
