672a08fe30e5f5818feaa4a4631208f208a8d330
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-11-07 00:02:31 +0900

    [level 3] Title: 정수 삼각형, Time: 7.77 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,17 +1,26 @@
 function solution(triangle) {
-    const N = triangle.length;
-    const dp = Array.from(Array(N), ()=> Array(N).fill(0));
-    dp[0][0] = triangle[0][0];
-    dp[1][0] = triangle[0][0] + triangle[1][0];
-    dp[1][1] = triangle[0][0] + triangle[1][1];
-    
-    for(let i = 1; i<N; i++) {
-        dp[i][0] = dp[i-1][0] + triangle[i][0];
+    var answer = 0; 
+    const dp = Array.from({length: triangle.length}, () => []);
 
-        for(let k = 1; k<i+1; k++) {
-            dp[i][k] = Math.max(dp[i-1][k-1], dp[i-1][k]) + triangle[i][k];
+    // triangle[i][j] 의 최대는
+    // i가 0 이거나, i 번째 배열의 끝이면 무조건 정해져있고,
+    // 그게 아닌 경우에는 dp[i][j] = [i-1][j-1]과 [i-1][j] 둘 중 큰 것 + [i][j] 인 것        
+    for(let i = 0 ; i<triangle.length; i++) {
+        if(i === 0) {
+            dp[i].push(triangle[i][0]);
+        }else if(i === 1) {
+            dp[i].push(triangle[i][0]+triangle[i-1][0]);
+            dp[i].push(triangle[i][1]+triangle[i-1][0]);
+        }else {
+            //3번째 줄부터는 이제 규칙 적용
+            const lastIdx = triangle[i].length-1;
+            dp[i].push(triangle[i][0]+dp[i-1][0])
+            for(let j = 1; j<lastIdx; j++) {
+                dp[i].push(Math.max(dp[i-1][j-1], dp[i-1][j]) +triangle[i][j]);
+            }
+            dp[i].push(triangle[i][lastIdx]+dp[i-1][lastIdx-1]);
         }
     }
-    const result = Math.max(...dp[triangle.length-1])
-    return result;
+    answer = Math.max(...dp[triangle.length-1]);
+    return answer;
 }
