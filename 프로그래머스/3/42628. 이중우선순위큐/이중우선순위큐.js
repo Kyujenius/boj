260e9a4767148c106993ece012915606fd38ec17
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-11-07 11:20:17 +0900

    [level 3] Title: 이중우선순위큐, Time: 937.54 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,46 +1,4 @@
 function solution(operations) {
     var answer = [];
-    operations.forEach((oper)=> {
-        operation(oper);
-    })
-    function operation(oper) {
-        
-        const operString = oper.split(" ");
-        const operation = operString[0];
-        const number = operString[1];
-        
-        switch(operation){
-            case "I":
-                answer.push(number);
-                break;
-            case "D":
-                if(operString[1] == 1){
-                    if(answer.length >0) {
-                        const numAnswer = answer.map((num)=> parseInt(num))
-                        const maxNum = Math.max(...numAnswer);
-                        const maxIdx = answer.indexOf(maxNum.toString());
-                        answer.splice(maxIdx,1);    
-                    }
-                    
-                }else if(operString[1] == -1){
-                    if(answer.length >0) {
-                        const numAnswer = answer.map((num)=>parseInt(num))
-
-                        const minNum = Math.min(...numAnswer);
-                        // console.log("minNUm", minNum);
-                        const minIdx = answer.indexOf(minNum.toString());
-                        answer.splice(minIdx,1);
-                    }
-                }
-                break;                
-        }
-        // console.log(oper, answer);
-            
-    }
-    if(answer.length == 0) {
-     answer = [0,0]   
-    }else {
-        answer = [Math.max(...answer),Math.min(...answer)]
-    }
     return answer;
 }
