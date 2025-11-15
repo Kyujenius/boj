357efb61264ab31f1b6a4eeb5f5eee7ccfd0f40d
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-11-15 21:00:12 +0900

    [level 2] Title: 배달, Time: 23.63 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,8 +1,6 @@
 function solution(N, road, K) {
     var answer = 0;
-    const graph = {}
-    //road 값을 이용해서 from to dist 로 일단 각각 만들고, 각 값으로 
-    //graph를 만든다. 
+    const graph = {};
     road.forEach((value) => {
         const [from,to,dist] = value;
         if(graph[from] == undefined) {
@@ -11,40 +9,32 @@ function solution(N, road, K) {
             graph[from].push({to:to, dist:dist});
         }
         if(graph[to] == undefined) {
-            graph[to] = [{to:from, dist:dist}]
+            graph[to] = [{to:from, dist:dist}];
         }else {
             graph[to].push({to:from, dist:dist});
         }
     })
-    graph[0] = [{to: 1, dist: 0}]
-    console.log(graph);
-
+    const queue = [{to:1,dist:0}];
     const accDist = Array(N+1).fill(Infinity);
     // console.log(accDist);
-    //1번 마을을 기준으로 3보다 다 적은 부분의 거리를 재자.
     accDist[1] = 0;
-    const queue = [{to:1, dist:0}];
-    while(queue.length > 0) {
-        const {to} = queue.shift();
-        //이미 갈 곳의 누적 거리 + 가기 위해 피룡한 거리
+    while(queue.length >0) {
+        const {to} = queue.pop();
         graph[to].forEach((next) => {
-            const acc = accDist[to] + next.dist;
-            //이미 갈 곳의 누적 거리 + 가기 위해 피룡한 거리
-            if(accDist[next.to] > acc) {
-                accDist[next.to] = acc;
+            const toDist = accDist[to] + next.dist;
+            if(accDist[next.to] > toDist) {
+                accDist[next.to] = toDist;
                 queue.push(next);
             }
         })
-      
     }
-    // console.log(accDist)
+    // console.log(accDist);
     
     accDist.forEach((value)=>{
-        if(value<=K){
-        answer++;
-    }
-    }
-                   )
+        if(value <=K) {
+            answer++;
+        }
+    })
 
     return answer;
 }
