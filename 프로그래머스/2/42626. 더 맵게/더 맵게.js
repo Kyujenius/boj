1cad50d8c2933640a989a4585f963bec34d9049f
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-11-15 17:34:14 +0900

    [level 2] Title: 더 맵게, Time: 583.67 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -2,89 +2,81 @@ function solution(scoville, K) {
     var answer = 0;
     const minHeap = new MinHeap();
     scoville.forEach((value) => minHeap.push(value));
-    let count =0;
-    while(true){
+    // console.log(minHeap.heap);
+    if(minHeap.heap[0] >= K) return 0;
+    while(true) {
         const a = minHeap.pop();
         const b = minHeap.pop();
-        if(a >= K) {
-            return count;
-        }
-        if(a == undefined) {
-            return -1;
-        }
-        if(b == undefined) {
-            return -1;
-        }
+        if(a == undefined || b == undefined) return -1;
         const newFood = a + b*2;
         minHeap.push(newFood);
-        count++;
+        answer++;
+        // console.log(minHeap.heap);
         if(minHeap.heap[0] >= K) {
             break;
-         }
-    }
-    return count;
+        }
+
+    }       
+    return answer;
 }
 
 class MinHeap {
     constructor() {
         this.heap = [];
     }
-    swap(a,b) {
-        [this.heap[a],this.heap[b]] = [this.heap[b],this.heap[a]];
-    }
-    push(input) {
-        this.heap.push(input);
+    push(value) {
+        this.heap.push(value);
         this.bubbleUp();
     }
     pop() {
-        if(this.heap.length ==1) {
-            return this.heap.pop();
-        }
-        if(this.heap.length ==0) {
-            return undefined;
-        }
-        const popped = this.heap[0];
+        if(this.heap.length == 0) return undefined;
+        if(this.heap.length == 1) return this.heap.pop();
+        const popped =this.heap[0];
         this.heap[0] = this.heap.pop();
         this.bubbleDown();
         return popped;
     }
+    swap(a,b) {
+        [this.heap[a],this.heap[b]] = [this.heap[b], this.heap[a]];
+    }
     bubbleUp() {
         let index = this.heap.length-1;
-        
         let parentIdx = Math.floor((index-1)/2);
-        while(this.heap[parentIdx] > this.heap[index]) {
-            this.swap(parentIdx,index);
+        
+        while(index > 0 && this.heap[index] < this.heap[parentIdx]){
+            this.swap(index,parentIdx);
             index = parentIdx;
             parentIdx = Math.floor((index-1)/2);
         }
     }
-    
     bubbleDown() {
         let index = 0;
-        let lastIdx = this.heap.length-1;
+        let swapIdx = null;
+        const lastIdx = this.heap.length -1;
         while(true) {
             let leftIdx = index *2 +1;
             let rightIdx = index *2 +2;
-            let swapIdx = null;
-            //왼쪽 존재할 때
+            let leftElement = this.heap[leftIdx];
+            let rightElement = this.heap[rightIdx];
+            let element = this.heap[index];
             if(lastIdx >= leftIdx) {
-                if(this.heap[leftIdx] < this.heap[index]) {
+                if(leftElement < element) {
                     swapIdx = leftIdx;
                 }
             }
-            
             if(lastIdx >= rightIdx) {
-                if(swapIdx !== null && this.heap[rightIdx] < this.heap[index] && this.heap[rightIdx] < this.heap[leftIdx]
-                   || (swapIdx == null && this.heap[rightIdx] < this.heap[index])) {
-                    //왼쪽이 없는 상황에, 부모보다 오른쪽이 더 작은 경우
-                    //왼쪽이 있는데도, 둘이 비교했을 때 오른 쪽이 더 큰 경우
+                if((swapIdx !== null && rightElement < element && rightElement <leftElement )||
+                   (swapIdx == null && rightElement < element)
+                  ) {
                     swapIdx = rightIdx;
                 }
             }
-            
-            if(swapIdx == null) break; 
+            if(swapIdx == null) {
+                break;
+            }
             this.swap(index, swapIdx);
             index = swapIdx;
+            swapIdx = null;
         }
     }
 }
