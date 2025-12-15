de30c7d651b5b28871df3143bf9454526b5f441b
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-12-15 19:31:04 +0900

    [level 2] Title: [3차] 압축, Time: 0.54 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,34 +1,36 @@
 function solution(msg) {
-    let answer = [];
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+   var answer = [];
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
     const alphabetArray = alphabet.split("");
     const dictionary = new Map();
-    alphabetArray.forEach((value, index) => {
-        dictionary.set(value.toUpperCase(), index+1);
+    alphabetArray.forEach((value,index) => {
+        dictionary.set(value.toUpperCase(),index+1);
     })
-    // console.log(dictionary);
-    let startIdx = 0;
     const length = msg.length;
+    let startIdx = 0;
+    
     while(startIdx < length) {
-        let indexingMsg = "";
-        let count = 0;
-        for(let i = 1; i<=length; i++) {
-            const foundMsg = msg.slice(startIdx,startIdx+i);
-            // 1. 사전에서 찾았을 때,
-            if(dictionary.has(foundMsg)) {
-                indexingMsg = foundMsg;
+        let idxWord = "";
+        let count = 0 ;
+        for(let i = 1 ; i<=length; i++) {
+            const foundWord = msg.slice(startIdx, startIdx + i);
+            //갖고 있으면 count 하나 더 올리기
+            // console.log("찾는 단어", foundWord)
+
+            if(dictionary.has(foundWord)) {
+                idxWord = foundWord;
                 count = i;
             }else {
-            // 2. 사전에서 못찾았을 때, 
-                console.log(foundMsg ,"추가");
-                dictionary.set(foundMsg, dictionary.size + 1)
+                // console.log("사전 추가", foundWord);
+                dictionary.set(foundWord,dictionary.size +1);
                 break;
-            }    
-        }
+            }
+         }
         startIdx += count;
-        answer.push(dictionary.get(indexingMsg));
+        answer.push(dictionary.get(idxWord));
     }
     // console.log(dictionary);
 
+ 
     return answer;
 }
