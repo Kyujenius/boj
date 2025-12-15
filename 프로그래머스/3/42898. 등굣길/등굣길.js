8a2d5be16a7ce9ab883c2417ca261e99f4ded319
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-12-15 20:42:54 +0900

    [level 3] Title: 등굣길, Time: 2.91 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,34 +1,30 @@
 function solution(m, n, puddles) {
-    const maps = Array.from({length:n},() => Array(m).fill(-1));
-    const mod = 1000000007;
-    for(let i = 0; i<m; i++) {
-        maps[0][i] = 1;
-    }
-    for(let i = 0; i<n; i++) {
-        maps[i][0] = 1;
-    }
+    var answer = 0;
+    const map = Array.from({length:n}, ()=> Array(m).fill(1));
+    // console.log(map);
     puddles.forEach((value)=> {
         const [a,b] = value;
-        maps[b-1][a-1] = 0;
-        if(a==1) {
-            for(let i = b-1; i<n; i++) {
-                maps[i][0] = 0;
-            }   
+        map[b-1][a-1] = 0;
+        if(a == 1) {
+            for(let i =b-1; i< n; i++) {
+                map[i][0] = 0;
+            }
         }
-        if(b==1) {
-            for(let i = a-1; i<m; i++) {
-                maps[0][i] = 0;
-            }   
+        if(b == 1) {
+            for(let i =a-1; i< m; i++) {
+                map[0][i] = 0;
+            }
         }
     })
+    // console.log(map);
+    
     for(let i = 1; i<m; i++) {
-        for(let j =1; j<n; j++) {
-            if(maps[j][i] == -1) {
-                maps[j][i] = (maps[j-1][i] + maps[j][i-1])% mod           
+        for (let j = 1 ; j<n; j++) {
+            if(map[j][i] !== 0) {
+                map[j][i] = (map[j-1][i] + map[j][i-1]) % 1000000007;    
             }
         }
     }
-    // console.log(maps);
-
-    return maps[n-1][m-1];
+    // console.log(map);
+    return map[n-1][m-1];
 }
