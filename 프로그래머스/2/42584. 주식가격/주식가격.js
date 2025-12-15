c7fa9587d1bb628560015dc0e6759d3eacf8f82a
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-12-15 19:37:04 +0900

    [level 2] Title: 주식가격, Time: 32.83 ms, Memory: 44.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,24 +1,19 @@
 function solution(prices) {
     var answer = [];
+    
     prices.forEach((value,index,array) => {
-        //value 보다 더 큰 가격이 있는지 확인해야함.
-        // console.log(value,index,array);
         let count = 1;
-        for(let i = index+1; i < array.length-1; i++) {
+        for(let i = index+1 ; i < array.length-1; i++) {
             if(value <= array[i]) {
-                count++;
+                count ++;
             }else {
                 break;
             }
-        }
-        if(array.length-1 == index) {
-            answer.push(0);    
-        }else {
-            answer.push(count);    
-        }
-        
 
-
+        }
+        answer.push(count);
     })
+    answer.pop();
+    answer.push(0);
     return answer;
 }
