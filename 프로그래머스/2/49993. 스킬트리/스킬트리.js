889682b79fcd5746f6d97cdec5bf058a7c012158
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-12-16 22:45:54 +0900

    [level 2] Title: 스킬트리, Time: 9.19 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,45 +1,37 @@
 function solution(skill, skill_trees) {
     var answer = 0;
-    const skillObj = {};
     const skillArray = skill.split("");
-    skillArray.forEach((value,index,array)=> {
-        for(let i = 0; i<index; i++) {
-            if(skillObj[value] == undefined) {
-                skillObj[value] = [array[i]];
-            }else {
-                skillObj[value].push(array[i]);
+    const skillBook = {};
+    skillArray.forEach((value, index, array) => {
+        for(let i = 0 ; i<index; i++) {
+            if(skillBook[value] == undefined) {
+                skillBook[value] = [];    
             }
+            skillBook[value].push(array[i]);    
+        }
+        
+    })
+    console.log(skillBook);
+    
+    skill_trees.forEach((skillTree,index,array)=> {
+        let isSuccess = true;
+        for(let i= 0; i<skillTree.length; i++) {
+            //그냥 여기서 B를 만날 시에(스킬북에 있는 거면) 그 앞단에, 다 있는지 확인
+            if(skillBook[skillTree[i]] !== undefined) {
+                const shouldHaveSkill = skillBook[skillTree[i]];
+                // 스킬북에 있어야하는 모든 스킬들을 잘라서 봤을 때 -1이 아니면 (존재하면) 통과
+                console.log(skillBook[skillTree[i]], "검증할 놈: ",skillTree.slice(0,i), )
+                if(shouldHaveSkill.some((value)=> skillTree.slice(0,i).indexOf(value) == -1)) {
+                    console.log("응 실패")
+                    isSuccess = false;
+                    break;
+                    
+              }
+            }
+        }
+        if(isSuccess) {
+            answer++;
         }
     })
-    console.log(skillObj);
-    //이제 스킬트리 하나씩 돌아가면서, 각 스킬트리 내에서 또 확인하면서 조건 충족하는지 확인하기!
-    for(let i = 0 ; i<skill_trees.length; i++) {
-        const tree = skill_trees[i];
-        let isGoodCharacter = true;
-        let badPatternChecker = [];
-        for(let j = 0; j<tree.length; j++) {
-            // 무조건 있어야 하는 스킬들
-            const shouldHaveSkill = skillObj[tree[j]];
-            if(shouldHaveSkill !== undefined) {
-                badPatternChecker = [...badPatternChecker, ...shouldHaveSkill]
-            }
-            
-            //있으면 안 되는 걸 만나버리면 이제 아웃!
-            if(badPatternChecker.some((value)=> value == tree[j])) {
-                console.log(tree, badPatternChecker,"안에 있는 건 있으면 안 되는데 있네..? :" ,tree[j]);
-                isGoodCharacter = false;
-                break;
-            }
-            
-            //있어야 하는 게 없으면 아웃!
-            const treeSlice = tree.slice(0,j+1);
-            if(badPatternChecker.some((value) => treeSlice.indexOf(value) == -1)) {
-                console.log(tree, badPatternChecker,"가 안에 있어야하는데 없네..? :" ,treeSlice);
-                isGoodCharacter = false;
-                break;
-            }
-        }
-        if(isGoodCharacter) answer++;
-    }
-    return answer;
+    return answer; 
 }
