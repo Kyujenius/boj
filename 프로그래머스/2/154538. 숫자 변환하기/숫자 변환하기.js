84687ad645c4406df6c70c1b3c1cc63fdae6b2ae
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-12-17 21:12:02 +0900

    [level 2] Title: 숫자 변환하기, Time: 477.27 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,33 +1,22 @@
 function solution(x, y, n) {
     var answer = 0;
-
-    const obj = {};
-    obj[0]=[x];
-    const set = new Set();
-    for(let i = 0 ; i<13; i++) {
-        const array= obj[i];
-        const newArray = [...array.map((value) => {
-            if(!set.has(value + n) {
-               return value +n;
-               }}), ...array.map((value)=>{if(!set.has(value*2)) {
-                          return value *2;
-                          }}),...array.map((value) => {if(!set.has(value*3)) {
-                          return value*3;
-                          }})] ;
-        obj[i+1] = newArray;
-    }
-    // console.log(obj);
-    for(let i = 0; i<13; i++) {
-        const array= obj[i];
-        // console.log(array);
-        if(array[0] > y) {
-            return -1;
-        }else if(array.some((value) => value == y)) {
-            return i;
+    const visited = new Set();
+    const queue = [];
+    queue.push([x,0]);
+    let index = 0;
+    while(index < queue.length) {
+        const [input, count] = queue[index];
+        if(input == y) return count;
+        const newInput = [input+n, input*2, input*3];
+        if(!visited.has(input) && input < y) {
+            for(let i = 0 ; i<3; i++) {
+                queue.push([newInput[i],count+1]);    
+                visited.add(input);
+            }
         }
 
+        index++
+        // console.log(queue);
     }
-    
-    
-    return answer;
+    return -1;
 }
