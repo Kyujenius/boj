9ebb4d7b222e2895379618e9f62096d318d5979f
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-12-18 21:38:55 +0900

    [level 3] Title: 숫자 게임, Time: 69.11 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -1,30 +1,19 @@
 function solution(A, B) {
     var answer = 0;
-    A.sort((a,b)=>b-a);
-    B.sort((a,b)=>b-a);
-    console.log(A,B);
-    let aIndex = 0;
+    A.sort((a,b) =>  b-a)
+    B.sort((a,b) =>  b-a)
     let leftIndex = 0;
     let rightIndex = B.length-1;
-    
-    //안되면 비길 때도 고려해야함
+    let count =0 ;
     while(leftIndex <= rightIndex) {
-        //B가 더 클 때,
-        if(A[aIndex] < B[leftIndex]) {
-            // console.log("B가 더커");
+        if(A[count] < B[leftIndex]) {
+            // console.log(count, leftIndex,A[count], B[leftIndex]);
+            answer++;
             leftIndex++;
-            answer += 1;
         }else {
-            if(A[aIndex] >= B[rightIndex]){
-                // console.log("A가 더 크거나 같아버려");
-                rightIndex--;
-            }else {
-                rightIndex--;
-                answer+= 1;
-            }
+            rightIndex--;
         }
-        //언제나 aIndex 는 증가함.
-        aIndex++;
+        count++;
     }
     return answer;
 }
