708194a158031469d43ac96677a8d08c4f71c718
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-12-21 22:49:07 +0900

    [level 3] Title: 단속카메라, Time: 23.35 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -0,0 +1,16 @@
+function solution(routes) {
+    var answer = 0;
+    routes.sort((a,b)=>a[1] - b[1])
+    
+    
+    let lastIdx = -30001;
+    // console.log(routes);
+    routes.forEach(([startRoute,endRoute])=> {
+        if(lastIdx < startRoute) {
+            lastIdx = endRoute;
+            answer++;
+        }
+    })
+    
+    return answer;
+}
