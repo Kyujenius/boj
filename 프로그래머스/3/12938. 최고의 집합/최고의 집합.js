e7fe23f855caf07d035b19f5deac970d08f88aeb
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-12-21 21:45:40 +0900

    [level 3] Title: 최고의 집합, Time: 0.47 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -1,20 +1,17 @@
 function solution(n, s) {
     var answer = [];
-    // 최고집합 : 합이 S 가 되는 집합, 이걸 만족하면서 원소 곱이 최대가 되는 집합        
-    if(s < n) return [-1];
-    if(s % n === 0) {
-        console.log(s%n);
-        for(let i =0  ; i <n; i++) answer.push(s/n);
-        return answer;
+    const k = s % n;
+    if(s<n) return [-1];
+    const length = Math.floor(s / n);
+
+    for(let i = 0 ; i<n-k; i++) {
+        answer.push(length);
     }
-    const k = s % n ;
-    for(let i =0; i < n; i++) {
-        answer.push(Math.floor(s/n));
-    };
-    // console.log(k);
-    for(let i = n-k; i<n; i++) {
-        answer[i] +=1;
+
+    for(let i = 0 ; i<k; i++) {
+        answer.push(length+1);
     }
+        
     
     return answer;
 }
