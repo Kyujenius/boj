fdc1a56d24ac633ea6148df7a6a0d487feaee536
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-12-25 15:58:18 +0900

    [level 3] Title: 베스트앨범, Time: 6.32 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,35 +1,4 @@
 function solution(genres, plays) {
     var answer = [];
-    const genreObj = {};
-    genres.forEach((genre,index,array)=> {
-        if(genreObj[genre] == undefined) {
-            genreObj[genre] = {array : [[plays[index],index]],
-                              total: plays[index]};
-        }else {
-            genreObj[genre].array.push([plays[index],index]);
-            genreObj[genre].total += plays[index];
-        }
-    });
-    
-    for(const key in genreObj) {
-        genreObj[key].array.sort((a,b)=>{
-            if(a[0] > b[0]) return -1;
-            if(a[0] < b[0]) return 1;
-            return a[1] - b[1];
-        })
-    }
-    const genreEntries = Object.entries(genreObj);
-    
-    genreEntries.sort((a,b)=>b[1].total - a[1].total);
-    console.log(genreEntries);
-    genreEntries.forEach((genreEntry)=> {
-        const genre = genreEntry[0];
-        const genreObj = genreEntry[1];
-        if(genreObj.array[0] !== undefined) answer.push(genreObj.array[0][1]);
-       if(genreObj.array[1] !== undefined) answer.push(genreObj.array[1][1]);
-        
-    })
-    
-    
     return answer;
 }
