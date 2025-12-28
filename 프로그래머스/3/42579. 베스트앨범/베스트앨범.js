ac676e423f1d5b8866c8a1be8b29e1b28766b31a
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-12-28 20:46:43 +0900

    [level 3] Title: 베스트앨범, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,4 +1,28 @@
 function solution(genres, plays) {
     var answer = [];
+    const genreObj = {};
+    genres.forEach((genre,index,array)=> {
+        if(genreObj[genre] == undefined) {
+            genreObj[genre] = {play: [[plays[index], index]], total: plays[index]};
+        }else {
+            genreObj[genre].play.push([plays[index], index]);
+            genreObj[genre].total += plays[index];
+        }
+    })
+    // console.log(genreObj);
+    const genreEntries = Object.entries(genreObj)
+    genreEntries.sort((a,b)=>b[1].total - a[1].total);
+    // console.log(genreEntries);
+    genreEntries.forEach((genreEntry)=> {
+        genreEntry[1].play.sort((a,b)=>b[0]-a[0]);
+        // console.log(genreEntry[1].play);
+        if(genreEntry[1].play[0] !== undefined) {
+            answer.push(genreEntry[1].play[0][1]);
+        }
+        if(genreEntry[1].play[1] !== undefined) {
+            answer.push(genreEntry[1].play[1][1]);
+        }
+
+    })
     return answer;
 }
