dadefbc23d6556e19dd66694875def25f81a872a
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-01-02 19:28:25 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 74.82 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,49 +1,43 @@
 function solution(n, wires) {
     var answer = -1;
-    //wires 에서 하나를 지워보고 지운 것을 기준으로 set을 두개를 둬서, DFS 맹키로 쭉 다 넣어버린다?  
     const answerCandidate = [];
-    const newWires = wires;
-    for(let i = 0 ; i<n; i++) {
-        //하나 지우기
-        const map = {};
-
-        wires.forEach(([from,to], index)=> {
-            if(i === index) return;
-            if(map[from] == undefined) {
-                map[from] = [to]
+    for(let i = 0; i<wires.length; i++) {
+        const wireObj = {};
+        const aSet = new Set();
+        wires.forEach(([from,to],index,array)=>{
+            if(index === i) return;
+            if(wireObj[from] == undefined) {
+                wireObj[from] = [to]
             }else {
-                map[from].push(to);
+                wireObj[from].push(to)
             }
 
-            if(map[to] == undefined) {
-                map[to] = [from]
+            if(wireObj[to] == undefined) {
+                wireObj[to] = [from]
             }else {
-                map[to].push(from);
+                wireObj[to].push(from)
             }
         })
-        // console.log(map);
-        //맵 새로 만든 뒤, Set 두개 둬서 비교하기
-        const aSet = new Set();
+        console.log(wireObj);
+        const wireEntries = Object.entries(wireObj);
+        const firstKey = wireEntries[0][0];
         
-        const mapEntries = Object.entries(map);
-        const firstKey =  mapEntries[0][0];
-        const queue = [];
-        queue.push(firstKey);
+        const queue = [firstKey];
+        
         while(queue.length>0) {
-            const from = queue.shift();
-            //이제 연결된 배열들 중에 Set 에 넣고
-            map[from].forEach((to)=>{
+            const key = queue.shift();
+            wireObj[key].forEach((to)=> {
                 if(!aSet.has(to)) {
-                    queue.push(to);
                     aSet.add(to);
-                }
+                    queue.push(to);
+                }       
             })
         }
-        answerCandidate.push(Math.abs((n - aSet.size) - aSet.size));
+        
+        answerCandidate.push(Math.abs((n - aSet.size) - aSet.size))
     }
     // console.log(answerCandidate)
-   
-    answer = Math.min(...answerCandidate)
     
+    answer = Math.min(...answerCandidate)
     return answer;
 }
