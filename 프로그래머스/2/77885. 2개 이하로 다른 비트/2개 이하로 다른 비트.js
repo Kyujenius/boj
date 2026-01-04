691c4ad380aedb07226d597820b491440a280ec7
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-01-04 19:44:42 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 290.21 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -1,17 +1,19 @@
 function solution(numbers) {
     var answer = [];
-    numbers.forEach((number)=> {
-        if(number % 2 == 0) {answer.push(number+1);}
-        else {
-            let stringNumber = '0'+ number.toString(2);
-            const zeroIndex = stringNumber.lastIndexOf('0');
-            const stringNumberArr = stringNumber.split('');
-            stringNumberArr[zeroIndex] = '1'
-            stringNumberArr[zeroIndex+1] = '0'
-            stringNumber = stringNumberArr.join('');
-            const newNumber = parseInt(stringNumber,2);
-            answer.push(newNumber);
+    numbers.forEach((number,index, array) => {
+        if(number % 2 === 0) {
+            answer.push(number +1);   
+        }else {
+            let twoString = number.toString(2);
+            twoString = '0' + twoString;
+            const twoStringArr = twoString.split("");
+            const lastZeroIndex = twoString.lastIndexOf('0');
+            twoStringArr[lastZeroIndex] = '1';
+            twoStringArr[lastZeroIndex+1] = '0';
+            const twoNumber = parseInt(twoStringArr.join(""),2);
+            answer.push(twoNumber);
         }
+        
     })
     return answer;
 }
