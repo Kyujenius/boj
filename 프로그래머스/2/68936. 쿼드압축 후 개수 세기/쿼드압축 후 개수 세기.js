ab35e1a637e3451a4a4334c1fac953e901024c0d
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-01-04 22:01:00 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 12.42 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,33 +1,32 @@
 function solution(arr) {
-    var answer = [];
+    var answer = [0,0];
     let n = arr.length;
-    let zeroCount = 0;
-    let oneCount = 0;
-    function divideNCount(startX,startY,n) {
-        const char = arr[startY][startX] ;
-        let isSame = true;
-        for(let i = startX ; i < startX + n ; i++) {
-            for (let j = startY; j <startY+n; j++) {
-                if(char !== arr[j][i]) {
-                    isSame = false;
+    
+    recursiveFn(0,0,n);
+    
+    function recursiveFn(startX, startY,n) {
+        const num = arr[startY][startX];
+        let isSuccess = true;
+        //처음부터 끝까지 살핀다.         
+        for(let i = 0 ; i<n; i++) {
+            for(let j = 0 ; j<n; j++) {
+                //가다가 다른 걸 확인해버린다? 
+                if(num !== arr[startY +i][startX + j]) {
+                    recursiveFn(startX, startY, n/2);
+                    recursiveFn(startX, startY+n/2, n/2);
+                    recursiveFn(startX+n/2, startY, n/2);
+                    recursiveFn(startX+n/2, startY+n/2, n/2);
+                    isSuccess = false;
+                    break;
                 }
             }
-            if(!isSame) break;
+         if(!isSuccess) {
+            answer[num]-=1;
+            break;
+         }
         }
-        if(isSame && char === 0) {
-            zeroCount++;
-        }
-        if(isSame && char === 1) {
-            oneCount++;
-        }
-        if(!isSame) {
-            divideNCount(startX,startY,n/2)
-            divideNCount(startX + n/2,startY,n/2)
-            divideNCount(startX + n/2,startY + n/2,n/2)
-            divideNCount(startX,startY + n/2,n/2)
-        }      
+        // console.log("끝",[startX,startY],n)
+        answer[num]+=1;
     }
-    divideNCount(0,0,n);
-    return [zeroCount,oneCount];
+    return answer;
 }
-
