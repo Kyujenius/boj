cc7c8ea2d035faaca60781e70939a7c7190258e9
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-01-06 00:09:57 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 29.48 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,27 +1,29 @@
 function solution(bridge_length, weight, truck_weights) {
     var answer = 0;
     const bridge = Array(bridge_length).fill(0);
-    while(true) {
-        const shiftedBridge = bridge.shift();
-        weight += shiftedBridge;
-        answer++;       
-        
-        //트럭을 넣을 수 있으면 넣기
-        if(weight - truck_weights[0] >= 0) {
-            const shiftedTruck = truck_weights.shift();    
-            bridge.push(shiftedTruck); 
-            weight -=shiftedTruck;
-        }else {
-            bridge.push(0);
+    // console.log(bridge);
+    const firstTruck = truck_weights.shift();
+    let currentWeight = 0;
+    bridge[bridge_length-1] = firstTruck;
+    currentWeight = firstTruck;
+    answer ++;
+    // 첫 트럭 올리고 시작
+    while(currentWeight>0) {
+        answer ++;
+        const shifted = bridge.shift();
+        currentWeight -= shifted;
+        //트럭이 남아있으면 넣어야지,
+        if(truck_weights.length > 0) {
+            //더 크면 못 올리고
+            if(weight - currentWeight < truck_weights[0]) {
+                bridge.push(0);
+            }else {
+            //작으면 올리고 하나씩 옮기고,
+                const newTruck = truck_weights.shift();
+                currentWeight += newTruck;
+                bridge.push(newTruck);
+            }
         }
-        
-
-        if(truck_weights.length === 0 && bridge.every((value) =>value === 0)) break;
     }
-
     return answer;
 }
-
-// 시간초를 어떻게 구현하지?
-
-// 언제 끝내지?
