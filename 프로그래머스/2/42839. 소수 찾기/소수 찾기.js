a63c282f5d0747deb01b901aaf4de8e6d460ca0b
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-01-05 23:36:43 +0900

    [level 2] Title: 소수 찾기, Time: 12.73 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,36 +1,39 @@
 function solution(numbers) {
     var answer = 0;
-    const splittedNumber = numbers.split("");
-    const n = splittedNumber.length;
-    const visited = Array(n).fill(false);
-    const result = new Set();
-    
-    recursiveFn("");
-    
-    function recursiveFn(currentString) {
-        const newNumber = parseInt(currentString);
-        if(newNumber > 0 && isPrime(newNumber) && !result.has(newNumber)) {
-            result.add(newNumber);
-            console.log(newNumber);
-        }
-        for(let i = 0; i<splittedNumber.length; i++) {
-            if(visited[i] ===false) {
+    const numArray = numbers.split("");
+    // console.log(numArray);
+    const visited = Array(numArray.length).fill(false);
+    // console.log(visited);
+    const set = new Set();
+    function dfs(currentNum) {
+        for(let i = 0 ; i<numArray.length; i++) {
+            if(visited[i] == false) {
                 visited[i] = true;
-                recursiveFn(currentString + splittedNumber[i])
+                const newNum = currentNum + numArray[i];
+                const num = parseInt(newNum)
+                if(isPrime(num) && !set.has(num)) {
+                    set.add(num);
+                    console.log(num)
+                    answer++
+                }
+                dfs(newNum);   
                 visited[i] = false;
             }
-        }    
-    }
-    
-    function isPrime(number) {
-        if(number < 2) return false;
-        if(number === 2) return true;
-        if(number === 3) return true;
-        for(let i = 2; i<=Math.sqrt(number); i++) {
-            if(number % i === 0) return false;
         }
-        return true;
+        
     }
-    
-    return result.size;
+    dfs("");
+    return answer;
+}
+
+function isPrime(number) {
+    const sqrtNum = Math.sqrt(number);
+    if(number <2) {
+        return false;
+    }
+    if(number ==2 || number ==3) return true;
+    for(let i = 2; i<=sqrtNum; i++) {
+        if(number % i === 0) return false;
+    }
+    return true;
 }
