1e26bdf6018f8d2cab2b75d6c8bcb6330530860c
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-01-11 21:27:34 +0900

    [level 3] Title: 불량 사용자, Time: 68.84 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,48 +1,39 @@
 function solution(user_id, banned_id) {
     var answer = 0;
-    const idSet = new Set();
-
-    function isMatched(user,ban) {
-        if(user.length !== ban.length) return false;
-        for(let i = 0 ; i<user.length; i++) {
-            if(user[i] !== ban[i] && ban[i] !== "*") {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    
-    const newArr = [];
+    //banned_id 에 맞는 user_id 들을 
     const visited = Array(user_id.length).fill(false);
-    function dfs(index) {
-        //끝까지 갔어? 
-        if(index === banned_id.length) {
-            const finalArr = user_id.filter((value,index) => {
-                if(visited[index] === true) {
-                    return true;
-                }else {
-                    return false;
-                }
-            })
-            idSet.add(finalArr.join(','));
-            return;
-        }
-        
-        //탐색 시작!
-        for(let i = 0 ; i<user_id.length; i++) {
-            if(visited[i]) continue;
-            
-            if(isMatched(user_id[i], banned_id[index])) {
-                visited[i] = true;
-                
-                dfs(index+1);
-                
-                visited[i] = false;
-            }
-        }
-    }
+    const set = new Set();
     
+    //visited 가 아니고, isMatched 도 true 면 dfs 한 번 더
+    function dfs(index){
+        if(index == banned_id.length) {
+            const filteredUser = user_id.filter((value,idx)=> visited[idx]);
+            const newUser = filteredUser.sort().join(',');
+            set.add(newUser);
+            return;
+        };
+        
+        user_id.forEach((value,idx)=> {
+            if(!visited[idx] && isMatched(value,banned_id[index])) {
+                visited[idx] = true;
+                dfs(index+1);
+                visited[idx] = false;
+            }     
+        })
+    }
     dfs(0);
-    return idSet.size;
+    
+    return set.size;
+}
+
+function isMatched(a,b) {
+    if(a.length !== b.length) return false;
+
+    for(let i =0; i<a.length; i++) {
+        if(a[i] !== b[i] && b[i] !== '*') {
+            return false;
+        }            
+    }
+
+    return true;
 }
