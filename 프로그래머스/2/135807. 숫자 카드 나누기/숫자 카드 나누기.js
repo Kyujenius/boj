40b9219017d62c0372c500512c6d05467dd1b780
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-01-11 19:54:27 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 71.92 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,38 +1,39 @@
 function solution(arrayA, arrayB) {
     var answer = 0;
+    const gcdA = arrayA.reduce((acc,cur)=> gcd(acc,cur));
+    const gcdB = arrayB.reduce((acc,cur)=>gcd(acc,cur));
     
-    const gcdA = arrayA.reduce((acc,cur)=>{
-        return getGCD(acc,cur);
+    console.log(gcdA,gcdB);
+    const aToB = arrayB.every((value)=> {
+        if(value % gcdA !== 0) {
+            return true;
+        }else {
+            return false;
+        }
     })
     
-    const gcdB = arrayB.reduce((acc,cur)=> {
-        return getGCD(acc,cur);
+    const bToA = arrayA.every((value)=> {
+        if(value % gcdB !== 0) {
+            return true;
+        }else {
+            return false;
+        }
     })
     
-    
-    // console.log(gcdA,gcdB);
-    
-    const isGCDATrue = arrayB.every((value)=> value % gcdA !== 0);
-    const isGCDBTrue = arrayA.every((value)=> value % gcdB !== 0);
-    
-    // console.log(isGCDATrue,isGCDBTrue)
-    if(isGCDATrue === true && isGCDBTrue === true) {
+    if(aToB == true && bToA == true) {
         return Math.max(gcdA,gcdB);
+    }else if(aToB == true && bToA == false) {
+        return gcdA;
+    }else if(aToB == false && bToA == true) {
+        return gcdB;
+    }else {
+        return 0;
     }
     
-    if(isGCDATrue === true && isGCDBTrue === false) {
-        return gcdA
-    }
-    if(isGCDBTrue === true && isGCDATrue === false) {
-        return gcdB
-    }
-    
-    function getGCD(a, b){
+    function gcd (a,b) {
         if(b === 0) return a;
-        
-        return getGCD(b, a % b);
-    };
+        return gcd(b,a % b);
+    }
     
     return answer;
-
 }
