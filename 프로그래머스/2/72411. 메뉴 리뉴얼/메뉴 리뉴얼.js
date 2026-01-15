ee36911336646f062b35e2a72ca851e035555fd5
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-01-15 20:54:11 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 31.80 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -1,47 +1,41 @@
 function solution(orders, course) {
     var answer = [];
-    function getCombinations (arr, selectNumber) {
+    function getCombinations (arr,selectNumber) {
+        if(selectNumber ===1) return arr.map((value)=> [value]);
         const results = [];
-        if(selectNumber ===1) return arr.map((value) => [value]);
-        arr.forEach((fixed,index,origin) => {
-            const rest = origin.slice(index+1);
-            const combinations = getCombinations(rest,selectNumber-1);    
-            const result = combinations.map((value => [fixed, ...value]));
+        arr.forEach((fixed,index,origin)=> {
+            const array = origin.slice(index+1);
+            const combinations = getCombinations(array, selectNumber-1);
+            const result = combinations.map((value)=> [fixed,...value]);
             results.push(...result);
-        }) 
-        
-        return results;
+        })
+        return results
     }
     
-    course.forEach((courseNumber) => {
-        const courseMap = new Map();
+    
+    course.forEach((courseNum) => {
+        const bestOrderMap = new Map();
+        //각 courseNum 이 getCombinations 의 숫자로 들어가서, Map 을 채운다.
         orders.forEach((order)=> {
-            const orderArr = order.split("");
-            if(courseNumber > orderArr.length) return;
-            const comb = getCombinations(orderArr, courseNumber);
-  
-            
-            comb.forEach((combinationKey)=> {
-                courseMap.set(combinationKey.sort().join(''), (courseMap.get(combinationKey.sort().join(''))||0)+1);        
+            const splittedOrder = order.split("").sort();
+            const combi = getCombinations(splittedOrder,courseNum);
+            combi.forEach((comb)=> {
+                const inputComb = comb.join('');
+                bestOrderMap.set(inputComb, (bestOrderMap.get(inputComb) || 0) +1);
             })
         })
-        
-        // console.log(courseMap);
-        
-        let candidateArr = [];
-        let maxNum = 0;
-        //제일 큰 놈들 뽑아다가 answer에 넣기,
-        courseMap.forEach((value,key,map)=> {
-            if(value > maxNum && value >= 2) {
-                maxNum = Math.max(maxNum, value);
-                candidateArr = [];
-                candidateArr.push(key);
-            }else if(value === maxNum) {
-                candidateArr.push(key);
+        let bestMenus = [];
+        let bestMenuNum = 0;
+        for(const [key,value] of bestOrderMap) {
+            if(value > bestMenuNum && value >=2) {
+                bestMenuNum = value;
+                bestMenus = [key]
+            }else if(value === bestMenuNum){ 
+                bestMenus.push(key);
             }
-        })
+        }
         
-        answer.push(...candidateArr);
+        answer.push(...bestMenus);
     })
     return answer.sort();
 }
