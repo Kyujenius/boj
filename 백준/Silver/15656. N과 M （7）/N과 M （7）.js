7f2d506f3623de978c7a6561e6c8e69947f59d22
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-01-25 18:18:05 +0900

    [Silver III] Title: N과 M (7), Time: 916 ms, Memory: 175488 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n')
+
+const [N,M] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number).sort((a,b)=>a-b);
+
+const visited = Array(N).fill(0);
+let result = '';
+const output = [];
+
+function dfs(count) {
+    if(count ===M) {
+        result += output.join(' ') + '\n';
+        return;
+    }
+    
+    for(let i = 0 ; i <N; i++) {
+        if(visited[i] < N) {
+            visited[i]++;
+            output.push(arr[i]);
+
+            dfs(count+1);
+            
+            visited[i]--;
+            output.pop();
+        }
+    }
+}
+
+dfs(0);
+console.log(result);
+
