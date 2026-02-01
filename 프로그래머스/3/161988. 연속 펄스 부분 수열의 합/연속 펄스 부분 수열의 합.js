6d3878b3a3a9cbba6fbdfab386cb0477b2dc853c
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-01 21:09:00 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 9.25 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
@@ -1,41 +1,31 @@
 function solution(sequence) {
     var answer = 0;
     const n = sequence.length;
+    let acc1 = 0;
+    let acc2 = 0;
     
-    let acc1 = 0 ;
-    let acc2 = 0 ;
+    let biggest1 = 0;
+    let biggest2 = 0;
     
-    let maxValue1 = 0;
-    let maxValue2 = 0;
-    
-    for(let i = 0; i<n; i++) {
-        const value1 = i % 2 === 0 ? sequence[i] : -sequence[i];
-        const value2 = -value1;
-        //누적합 
-        acc1 += value1;
+    for(let i = 0 ; i<n; i++) {
+        const val1 = i % 2 === 0 ? sequence[i] : -sequence[i];
+        const val2 = -val1;
         
-        //누적합이랑 maxValue 랑 비교
-        if(maxValue1 < acc1) {
-            maxValue1 = acc1;
-        }
-        //누적값보다 value1이 더 크면, 누적값 그냥 value1으로 초기화
-        if(value1 > acc1) {
-            acc1 = value1;
+        //가장 큰 누적합 vs 지금 값이랑 비교해서 누적합이 더 큰지 확인 
+        acc1 = acc1 + val1;
+        acc2 = acc2 + val2;
+        
+        if(acc1 < val1) {
+            acc1 = val1;
         }
         
-        acc2 += value2;
-
-        if(maxValue2 < acc2) {
-            maxValue2 = acc2;
+        if(acc2 < val2) {
+            acc2 = val2;
         }
         
-        if(value2 > acc2) {
-            acc2 = value2;
-        }
-                
-        
-        
+        biggest1 = Math.max(biggest1, acc1);
+        biggest2 = Math.max(biggest2, acc2);
         
     }
-    return Math.max(maxValue1,maxValue2);
+    return Math.max(biggest1,biggest2);
 }
