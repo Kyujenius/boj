66604450ce4a325d9d76273c0f67c04d7bd4022f
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-01 14:30:33 +0900

    [level 3] Title: 부대복귀, Time: 1097.04 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.js
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.js
@@ -1,47 +1,46 @@
 function solution(n, roads, sources, destination) {
     var answer = [];
-    const map = {};
+    const graph = {};
+    
     roads.forEach(([from,to])=> {
-        if(map[from] === undefined) {
-            map[from] = [to]
+        if(graph[from]===undefined) {
+            graph[from] = [to];
         }else {
-            map[from].push(to);
+            graph[from].push(to);
         }
         
-        if(map[to] === undefined) {
-            map[to] = [from];
+        if(graph[to]===undefined) {
+            graph[to] = [from];
         }else {
-            map[to].push(from);
+            graph[to].push(from);
         }
     })
+    // console.log(graph);
     
+    //[출발지, 거리] 
+    const queue = [[destination, 0]];
+    const dist = Array(n+1).fill(0);
+    dist[destination] = 0;
     
-    
-    const queue = [];
-    queue.push(destination);
-    
-    const visited = Array(n+1).fill(false);
-    const count =  Array(n+1).fill(-1);
-    
-    visited[destination] = true;
-    count[destination] = 0;
-    // console.log(map);
-    // console.log(visited);
-
-    while(queue.length>0) {
-        const from = queue.shift();
-        map[from].forEach((to) => {
-            if(visited[to] === false) {
-                queue.push(to);
-                count[to] = count[from] +1;
-                visited[to] = true;
+    while(queue.length > 0) {
+        const [from,distance] = queue.shift();
+        graph[from].forEach((to)=> {
+            if(dist[to] === 0 && to !== destination) {
+                dist[to] = distance+1;
+                queue.push([to,distance+1]);
             }
         })
     }
-    // console.log(visited);
-    // console.log(count);
-    answer = sources.map((source)=> {
-        return count[source];
+    // console.log(dist);
+    sources.forEach((source)=> {
+        if(source === destination) {
+            answer.push(0);
+        }else if(dist[source] === 0) {
+            answer.push(-1)    
+        }else {
+            answer.push(dist[source]);
+        }
+        
     })
     return answer;
 }
