f5c506afc0ccd4349a3727f2c0b126b407793d3b
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-13 20:42:32 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.89 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
@@ -1,50 +1,49 @@
-function solution(n, t, m, timeTableArr) {
-    var answer = '';
-    let busTime = 540;
-    const crew = timeTableArr.map((value)=> {
-        const [hour,minute] = value.split(":").map(Number);
-        const time = hour * 60 + minute;
-        return time
-    }).sort((a,b)=>a-b);
-    let answerStr = '';
+function solution(n, t, m, timetable) {
+    const crew = timetable.map((time)=> {
+        const [hour,minute] = time.split(":").map(Number);
+        const totalMinute = hour * 60 + minute;
+        return totalMinute;
+    }).sort((a,b)=> a-b);
     
+    // console.log(crew);
+    let busTime = 540;
     let crewIdx = 0;
-    let lastCrewNum = 0;
-    //각 버스가 오는 시간마다. 
-    for(let i = 0 ; i <n; i++) {
-        // console.log(`========${i} ==========`)
+    let lastCrewTime = 0;
+    let lastTime = 0;
+    //9시를 기준으로 각 t분마다. n 번, 태울 차가 온다.
+    for(let i = 0 ; i<n; i++) {
         let count = 0 ;
-        while(count < m && crewIdx < crew.length && crew[crewIdx] <= busTime) {
-            // console.log(`${crew[crewIdx]} 크루`);
-            lastCrewNum = crew[crewIdx];
-            count++;
+        
+        //각 크루들을 추적하는 CrewIdx 가 넘치지 않았고, 제 시간에 온 crew 들에 한해서, 버스가 태울 수 있는 바운더리 안이면 계속 태운다.
+        while(crewIdx < crew.length && crew[crewIdx] <= busTime  && count < m) {
+            // console.log(`lastCrewTime: ${crew[crewIdx]}`);
+            lastCrewTime = crew[crewIdx];
             crewIdx++;
+            count++;
         }
         
-        //막차면 이제 정해야지
-        if(i === n-1) {
-            //꽉 안 찼으면 그냥 차 올 때 타면 됩니다요
-            if(count < m) {
-                answerStr = busTime;
-                // console.log(`꽉 안차서 ${busTime} 등록`);
-            }
-            
-            //꽉 찼으면 그냥 마지막 시간보다만 1분 빨리타면 됩니다요
+        //마지막 버스면, 
+        if(i == n-1) {
+            //가득 채워서 왔으면 그 시간보다는 적게
             if(count === m) {
-                
-                answerStr = lastCrewNum-1;
-                // console.log(`꽉 차서 ${answerStr} 등록`);
+                // console.log(`가득 채워서 왔어`)
+                lastTime = lastCrewTime -1;
+            }else if(count < m) {
+                // console.log(`부족하게 왔어`)
+            //안 가득 찼으면 그냥 busTime에다가
+                lastTime = busTime;
+            }else {
+                // console.log("그럴 수 없어");
             }
-            
         }
-
+        
+        
         busTime += t;
     }
-            
-    const hour = Math.floor(parseInt(answerStr) / 60).toString().padStart(2,'0');;
-    const minute = (parseInt(answerStr) % 60).toString().padStart(2,'0');
-        
-    answer = `${hour}:${minute}`;
     
+    const hour = Math.floor(lastTime / 60).toString().padStart(2,'0');;
+    const minute = (lastTime % 60).toString().padStart(2,'0');
+    
+    const answer = `${hour}:${minute}`;
     return answer;
 }
