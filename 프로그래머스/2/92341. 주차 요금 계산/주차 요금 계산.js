2b293956ec6a2d26cb8321b028cd94f13f182c91
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-15 14:48:40 +0900

    [level 2] Title: 주차 요금 계산, Time: 3.71 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,47 +1,59 @@
 function solution(fees, records) {
-    var result = [];
-    const defaultTime = fees[0];
-    const defaultFee = fees[1];
-    const time = fees[2];
-    const timePerFee = fees[3];
+    var answer = [];
+    const [defaultTime, defaultFee, perTime, perFee] = fees;
+    // console.log(defaultTime, defaultFee, perTime, perFee);
+    
     const carObj = {};
-    records.forEach((record,index,array)=> {
-        const [time, carNumber,inOut]= record.split(" ");
-        const [hour,minute] = time.split(":");
-        let totalMinute = parseInt(hour) * 60 + parseInt(minute);
-        if(inOut === "IN") totalMinute = totalMinute * (-1);
-        if(!carObj[carNumber]) {
-            carObj[carNumber] = [totalMinute,inOut];
+    records.forEach((value)=> {
+        const [time, carNumber, INOUT] = value.split(" ");
+        const [hour, minute] = time.split(":").map(Number);
+        const totalTime = hour*60 + minute;
+        
+        if(carObj[carNumber] === undefined) {
+            if(INOUT === "IN") {
+                carObj[carNumber] = {
+                    totalTime: -totalTime,
+                    inOut: INOUT
+                };   
+            };
+            if(INOUT === "OUT") {
+                carObj[carNumber] = {
+                    totalTime: totalTime,
+                    inOut: INOUT
+                };
+            }
         }else {
-            carObj[carNumber][0] += totalMinute;
-            carObj[carNumber][1] = inOut;
+            if(INOUT === "IN") {
+                carObj[carNumber].totalTime -= totalTime
+                carObj[carNumber].inOut  = INOUT
+            }
+            if(INOUT === "OUT") {
+                carObj[carNumber].totalTime += totalTime
+                carObj[carNumber].inOut  = INOUT
+            }
         }
     })
     
-    //다 끝났는데 IN인 놈들 
     for(const carKey in carObj) {
-        if(carObj[carKey][1] == "IN") {
-            carObj[carKey][0] += 23*60 + 59;
-            carObj[carKey][1] = "OUT";
+        if(carObj[carKey].inOut === "IN") {
+            carObj[carKey].totalTime += (23*60 + 59);
+            carObj[carKey].inOut = "OUT";
         }
     }
     
-    console.log(carObj);
-    
-    const keys = Object.keys(carObj);
+    const keys =  Object.keys(carObj);
     keys.sort();
-    
-    keys.forEach((key)=> {
-        let answer = Infinity;
-        if(carObj[key][0] - defaultTime < 0) {
-            answer = defaultFee;
-        }else {
-            answer = defaultFee + Math.ceil((carObj[key][0] - defaultTime)/time) * timePerFee
+    // console.log(keys);
+    for(let i = 0 ; i<keys.length; i++){
+        const key = keys[i];
+        const finalTime = carObj[key].totalTime;
+        if(finalTime<=defaultTime){
+            answer.push(defaultFee);   
+        }else{
+            answer.push(defaultFee + (Math.ceil((finalTime-defaultTime) / perTime) * perFee))
         }
-        result.push(answer);
-    })
-    
         
-    
-    return result;
+    }
+    // console.log(carObj);
+    return answer;
 }
