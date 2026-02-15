753f232f19e6f95077966be7e5bef6678ead6347
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-15 17:53:22 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 21.68 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,53 +1,81 @@
 function solution(m, n, board) {
     var answer = 0;
-    const map = board.map((row)=>row.split(""));
-        
+    board = board.map((value)=> value.split(""));
+    
+    // console.log(is2x2);
+    
     while(true) {
-        //map 에서 패턴 있는지 개수 세서 bingoMap에 넣기
-        const bingoMap = Array.from({length: m},()=>Array(n).fill(0));
-
-        let bingoCount = 0;
-        for(let i = 0; i<m-1; i++){
-            for(let j = 0; j<n-1; j++) {
-                const char = map[i][j];
-                if((char === map[i+1][j])&&
-                   (char === map[i][j+1])&&
-                   (char === map[i+1][j+1]) && (
-                char !== 0)
+        const is2x2 = Array.from({length: board.length}, ()=> {
+            return Array(board[0].length).fill(0)
+        });
+        let count = 0;
+        
+        // 2x2 가 통하는 곳을 찾는다.
+        // 통한 부분에 한해서, 1로 표시한다.
+        for(let i= 0 ; i<board.length-1; i++) {
+            for(let j = 0; j<board[0].length-1; j++) {
+                const target = board[i][j];
+                
+                if((target === board[i+1][j])&&
+                  (target === board[i][j+1])&&
+                  (target === board[i+1][j+1]) &&
+                   (target !== '0')
                   ) {
-                    bingoMap[i][j] = 1;
-                    bingoMap[i][j+1] = 1;
-                    bingoMap[i+1][j] = 1;
-                    bingoMap[i+1][j+1] = 1;
-                    bingoCount+=1;
-                }
+                        if(is2x2[i][j] !== 1) {
+                           count++;
+                        }
+                        if(is2x2[i][j+1] !== 1) {
+                           count++;
+                        }
+                        if(is2x2[i+1][j] !== 1) {
+                           count++;
+                        }
+                        if(is2x2[i+1][j+1] !== 1) {
+                           count++;
+                        }
+                       is2x2[i][j] = 1;
+                       is2x2[i+1][j+1] = 1;
+                       is2x2[i][j+1] = 1;
+                       is2x2[i+1][j] = 1;
+                   }
             }
         }
-        //없으면 break
-        if(bingoCount === 0) break;
-        //있으면 그 bingoMap 을 통해서 다시 채우고 끝내기
-        // console.log(bingoMap)
-        for(let i = 0; i<n; i++) {
+        
+        
+    
+        // 통한 부분이 없으면 break;
+        if(count == 0) {
+            break;
+        }else {
+            answer += count;
+        }
+        
+        const newBoard = [];
+        // 1로 표시한 부분을 아래로 내리고, 안 채워진 부분에 한해서 0 으로 채운다.
+        for(let j =  0; j< board[0].length; j++) {
             const column = [];
             let zeroCount = 0;
-            for(let j = m-1; j>=0; j--) {
-                if(bingoMap[j][i] === 0) {
-                    column.push(map[j][i]);    
+            for(let i = board.length-1; i>=0; i--) {
+                if(is2x2[i][j] !== 1){
+                    column.push(board[i][j]);
                 }else {
                     zeroCount++;
-                    answer++;
                 }
             }
-            for(let k = 0; k<zeroCount; k++) {
-               column.push(0); 
+            for(let i = 0 ; i<zeroCount;i++) {
+                column.push('0');
             }
-            // console.log(column);
-            for(let k = 0; k<column.length; k++) {
-               map[column.length - k-1][i] = column[k]
-            }
-            // console.log(map);
+            newBoard.push(column);
         }
-
+        
+        
+        // console.log(board);
+        // console.log(newBoard);
+        for(let i = 0 ; i<board.length; i++) {
+            for(let j = 0 ; j<board[0].length; j++) {
+                board[i][j] = newBoard[board[0].length-1 - j][board.length -1-i];
+            }
+        }
         // break;
     }
     return answer;
