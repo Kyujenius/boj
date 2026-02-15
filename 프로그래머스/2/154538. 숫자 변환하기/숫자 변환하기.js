cfed8f6f5a6649950f0ed2fa46063de66506bfc8
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-15 14:48:57 +0900

    [level 2] Title: 숫자 변환하기, Time: 151.45 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,21 +1,37 @@
 function solution(x, y, n) {
-    var answer = 0;
+    const queue = [[x,0]];
     const visited = new Set();
-    const queue = [];
-    
-    queue.push([x,0]);
+    visited.add(x);
     let queueIdx = 0;
     while(queueIdx < queue.length) {
-        const [number, count] = queue[queueIdx];
-        if(number === y) return count;
-        const newNumber = [number+n, number*2, number*3];
-        for(let i = 0 ; i <3; i++) {
-            if(!visited.has(newNumber[i]) && newNumber[i] <= y) {
-                queue.push([newNumber[i],count+1]);
-                visited.add(newNumber[i]);
-            }
+        const [current, count] = queue[queueIdx];
+        
+        if(current === y) {
+            return count;
         }
-        // console.log(queue)
+        
+        const x3 = current * 3;
+        const x2 = current * 2;
+        const plusN = current +n;
+        
+        
+        if(plusN === y) return count+1;
+        if(plusN < y && !visited.has(plusN)) {
+            queue.push([plusN,count+1]);
+            visited.add(plusN);
+        }
+     
+        if(x2 === y) return count+1;
+        if(x2 < y && !visited.has(x2)) {
+            queue.push([x2,count+1]);
+            visited.add(x2);
+        }
+        
+        if(x3 === y) return count+1;
+        if(x3 < y && !visited.has(x3)) {
+            queue.push([x3,count+1]);
+            visited.add(x3);
+        }   
         queueIdx++;
     }
     return -1;
