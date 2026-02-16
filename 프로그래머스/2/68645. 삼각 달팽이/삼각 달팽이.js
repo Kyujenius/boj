502d514ccd889c5b26497115363ee0433716599c
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-17 01:47:13 +0900

    [level 2] Title: 삼각 달팽이, Time: 66.24 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,36 +1,48 @@
 function solution(n) {
     var answer = [];
-    const map = Array.from({ length: n }, (_, index) => Array(index + 1).fill(0));
-    let y = -1;
-    let x = 0;
-    let count = 1;
-    let number = n;
-    let mode = 'down' ;
-    while(number >= 0) {
-        for(let i = 0 ; i<number; i++) {
-            y++;
-            map[y][x] = count;
-            count++;
+    const arr = Array.from({length:n},()=> Array(n).fill(0));
+    const length = n;
+    // console.log(arr);
+    let startX = 0;
+    let startY = -1;
+    let count = 0;
+    for(let i = 0 ; i<length; i++) {
+        //direction 은 방향 0: 아래, 1은 오른쪽, 2는 대각선 위
+        const direction = i % 3;
+        
+        //방향이 아래일 때는,  
+        if(direction === 0) {
+            for(let j = 0; j<n; j++) {
+                count++;
+                startY++;
+                arr[startY][startX] = count;
+            }
         }
-        number--;
-        for(let i = 0; i<number; i++){
-            x++;
-            map[y][x] = count;
-            count++;
+        //방향이 오른쪽일 때는,
+        if(direction === 1) {
+            for(let j = 0; j<n; j++) {
+                count++;
+                startX++;
+                arr[startY][startX] = count;
+            }
         }
-        number--;
-        for(let i = 0; i<number; i++){
-            y--;
-            x--;
-            map[y][x] = count;
-            count++;
-
+        //방향이 왼쪽 위일 때는,
+        if(direction === 2) {
+             for(let j = 0; j<n; j++) {
+                count++;
+                startX--;
+                startY--;
+                arr[startY][startX] = count;
+            }
         }
-        number--;
+        n--;
     }
-    map.forEach((array)=> {
-        array.forEach((value)=> {
-            answer.push(value);
+    // console.log(arr);
+    arr.forEach((row)=> {
+        row.forEach((value)=> {
+            if(value !== 0) {
+                answer.push(value);
+            }
         })
     })
     return answer;
