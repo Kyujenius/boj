493c47805fe2715125437da0d39919cbe647a646
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-16 20:57:31 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 38.98 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,32 +1,44 @@
 function solution(queue1, queue2) {
     var answer = -1;
-    let sum1 = queue1.reduce((acc,cur)=>acc+=cur);
-    let sum2 = queue2.reduce((acc,cur)=>acc+=cur);
-    const total = sum1+sum2;
-    
-    // console.log(sum1,sum2,total);
-    if(total % 2 !== 0) return -1;
-    const target = total/2;
-    let leftPtr = 0;
+    const combinedQueue = [...queue1, ...queue2];
+    let leftSum = queue1.reduce((acc,cur)=> acc += cur, 0);
+    let rightSum = queue2.reduce((acc,cur)=> acc += cur, 0);
+    console.log(leftSum,rightSum);
+    // if(leftSum === rightSum)
+    let count = 0;
+    let leftPtr = 0 ;
     let rightPtr = queue1.length;
-    let count= 0;
-    
-    const combinedArr = [...queue1,...queue2];
-    while(count < queue1.length * 4) {
-        if(sum1 < target) {
-            sum1 += combinedArr[rightPtr];
-            rightPtr +=1;
-            rightPtr = rightPtr % (queue1.length *2)
-            count +=1;
-        }else if(sum1 > target){
-            sum1 -= combinedArr[leftPtr];
-            leftPtr +=1;
-            leftPtr = leftPtr % (queue1.length *2)
-            count +=1;
-        }else {
+    while(count < combinedQueue.length * 4) {
+        // if(leftPtr === rightPtr) {
+        //     return -1;
+        // }
+        //왼쪽 오른쪽이 같으면 stop 하고 카운트를 return 한다.
+        if(leftSum === rightSum) {
             return count;
         }
-    }
-    
+        //오른쪽이 크면 오른 쪽 거 포인터를 하나 옮기고, 
+        //leftSum 에는 더하기를 rightSum에는 빼기를 한다.
+        if(leftSum < rightSum) {
+            
+            const temp = combinedQueue[rightPtr];
+            
+            leftSum += temp;
+            rightSum -= temp;
+            // console.log(`오른쪽이 더 커서 ${temp} 왼쪽으로 옮김`);
+            // console.log(`leftSum :${leftSum}, rightSum: ${rightSum}`);
+            rightPtr = (rightPtr +1) % (combinedQueue.length);
+        }else {
+            //왼쪽이 크면 왼쪽 거 포인터를 하나 옮기고, 
+            //leftSum 에는 빼기를 rightSum에는 더하기를 한다.
+             const temp = combinedQueue[leftPtr];
+            leftSum -= temp;
+            rightSum += temp;
+            // console.log(`왼쪽이 더 커서 ${temp} 오른쪽으로 옮김`);
+            // console.log(`leftSum :${leftSum}, rightSum: ${rightSum}`);
+            leftPtr = (leftPtr +1) % (combinedQueue.length);
+        }
+
+        count++;
+    }    
     return answer;
 }
