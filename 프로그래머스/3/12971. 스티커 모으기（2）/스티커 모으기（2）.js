c43213b8cf28c55e8c30f9d5463ad5352ae72e2d
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-17 15:54:56 +0900

    [level 3] Title: 스티커 모으기(2), Time: 11.13 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -1,30 +1,27 @@
 function solution(sticker) {
     var answer = 0;
-    const n = sticker.length;
-    if(n ===1) return sticker[0];
-    //첫 번 째 것을 뜯은 경우
-    const dp1 = Array(n).fill(0);
-    
+    if (sticker.length === 1) return sticker[0];
+
+    const dp1 = Array(sticker.length).fill(0);
     dp1[0] = sticker[0];
     dp1[1] = sticker[0];
-    for(let i = 2; i<n-1; i++) {
-        dp1[i] = Math.max(sticker[i] + dp1[i-2], dp1[i-1]);    
-    }
-    
-    //
     const dp2 = Array(sticker.length).fill(0);
     dp2[0] = 0;
     dp2[1] = sticker[1];
     
-    for(let i = 2; i<n; i++) {
-        dp2[i] = Math.max(sticker[i] + dp2[i-2], dp2[i-1]);    
+    //첫 번째 것을 땔 경우
+    for(let i =2; i<sticker.length-1; i++) {
+        //i번째 스티커를 떼냐 vs 안 떼냐
+        dp1[i] = Math.max(sticker[i] + dp1[i-2], dp1[i-1]);
     }
     
-//     console.log(dp1);
-
-//     console.log(dp2)
+    //첫 번째 것을 안 뗄 경우
+    for(let i =2; i<sticker.length; i++) {
+        //i번째 스티커를 떼냐 vs 안 떼냐
+        dp2[i] = Math.max(sticker[i] + dp2[i-2], dp2[i-1]);
+    }
+    console.log(dp1,dp2);
+    answer = Math.max(dp1[sticker.length-2],dp2[sticker.length-1]);
     
-    answer = Math.max(dp1[n-2],dp2[n-1])
-
     return answer;
 }
