66f38f2a9da569723a70906dd028d5370f78e398
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-17 15:34:39 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 1066.97 ms, Memory: 320 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,29 +1,42 @@
 function solution(sequence, k) {
-    let left = 0;
-    let right = 0;
-    let sum = sequence[0]; // 첫 원소부터 시작
-    let answer = [0, sequence.length]; // 초기값: 가장 긴 길이로 설정해둠    let sum = sequence[left] + sequence[right];
-    let pointerLength = Infinity;
+    var answer = [];
+    let leftPtr = 0;
+    let rightPtr = 0;
+    let count = sequence[0];
+    let candidate = []
+    while(leftPtr <= rightPtr && rightPtr < sequence.length) {
+        //count 라는 값이 k 랑 같으면 스탑하면 안되고, cadidate 에 추가해야죠
+        if(count === k) {
+            candidate.push({distance : rightPtr-leftPtr, ptr: [leftPtr,rightPtr]});
+            rightPtr++;
+            count += sequence[rightPtr];
+        }else if(count < k) {
+            //k보다 작으면 오른쪽 포인터를 이동시키기
+            rightPtr++;
+            // console.log(`count(${count}) < k(${k}) : count 증가 : ${sequence[rightPtr]}`)
+            count += sequence[rightPtr];
 
-    while(left < sequence.length && right < sequence.length) {
-        if(sum < k) {
-            right++;
-            sum += sequence[right];
-        }
-        
-        if(sum > k) {
-            sum -= sequence[left];
-            left++;
-        }
-        if(sum === k) {
-            if(pointerLength > (right - left)) {
-                pointerLength = right - left;
-                answer= [left,right];
-            }
-            right++;
-            sum += sequence[right];
+        }else if(count > k) {
+            // console.log(`count(${count}) > k(${k}) : count 감소 : ${count}`)
+            count -= sequence[leftPtr];
+            leftPtr++;
         }
     }
-    console.log(answer);
+    // console.log(candidate);
+    
+    const candidateEntry = Object.entries(candidate);
+    // console.log(candidateEntry);
+    candidateEntry.sort((a,b)=> {
+        if(a[1].distance === b[1].distance) {
+            return a[1].ptr[0] - b[1].ptr[0];
+        }
+        return a[1].distance - b[1].distance;
+    })
+    
+    answer = candidateEntry[0][1].ptr;
     return answer;
 }
+
+//투 포인터 문제다!
+
+//
