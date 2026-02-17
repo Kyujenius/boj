6fc5bdc7b630e91a1db516f22cb4b063790c8524
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-17 16:51:16 +0900

    [level 2] Title: 시소 짝꿍, Time: 27.88 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,39 +1,24 @@
 function solution(weights) {
     var answer = 0;
-    const weightObj = {};
-    
-    weights.forEach((weight)=> {
-       if(weightObj[weight] == undefined) {
-            weightObj[weight] = 1;
-        }else {
-            weightObj[weight] += 1;  
-        }
+    const obj = {};
+    const obj2 = {};
+    const map = new Map();
+    weights.forEach((value)=> {
+        map.set(value,(map.get(value) || 0)+1);
     })
-    // console.log(weightObj);
-    
-    for(const weight in weightObj) {
-        const n = weightObj[weight];
-        const numWeight = parseInt(weight)
-        if(n >= 2) {
-            answer += (n*(n-1)) / 2
+    console.log(map);
+    for(const entry of map) {
+        if(entry[1] >= 2) {
+            const n = entry[1] ;
+            answer += n*(n-1)/2;
         }
-        const threeDevidedByTwo = numWeight * (3/2) ;
-        if(weightObj[threeDevidedByTwo]) {
-            const m = weightObj[threeDevidedByTwo]
-            answer += n*m
-        }
-        const two = numWeight * 2 ;
-        if(weightObj[two]) {
-            const m = weightObj[two]
-            answer += n*m
-        }
-        const fourDevidedThree = numWeight * (4/3);
-        if(weightObj[fourDevidedThree]) {
-            const m = weightObj[fourDevidedThree]
-            answer += n*m
-        };
-        
-        console.log(n,threeDevidedByTwo,two,fourDevidedThree)
+        const candidate = [entry[0] * (3/2), entry[0] * (4/3),entry[0] * 2];
+        candidate.forEach((value)=> {
+            if(map.get(value) !== undefined) {
+                answer += map.get(value) * entry[1];
+            }
+        })
     }
+    // console.log(obj);
     return answer;
 }
