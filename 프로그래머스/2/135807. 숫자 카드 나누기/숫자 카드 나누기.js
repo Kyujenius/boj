5fdc5e628b742286bda7f6d8e9aedcac92652259
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-19 19:22:30 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 47.51 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,39 +1,29 @@
 function solution(arrayA, arrayB) {
     var answer = 0;
-    const gcdA = arrayA.reduce((acc,cur)=> gcd(acc,cur));
-    const gcdB = arrayB.reduce((acc,cur)=>gcd(acc,cur));
+    const gcdA = arrayA.reduce((acc,cur) => gcd(acc,cur));
+    const gcdB = arrayB.reduce((acc,cur) => gcd(acc,cur));
+    // console.log(gcdA,gcdB);
     
-    console.log(gcdA,gcdB);
-    const aToB = arrayB.every((value)=> {
-        if(value % gcdA !== 0) {
-            return true;
-        }else {
-            return false;
-        }
-    })
-    
-    const bToA = arrayA.every((value)=> {
-        if(value % gcdB !== 0) {
-            return true;
-        }else {
-            return false;
-        }
-    })
-    
-    if(aToB == true && bToA == true) {
-        return Math.max(gcdA,gcdB);
-    }else if(aToB == true && bToA == false) {
-        return gcdA;
-    }else if(aToB == false && bToA == true) {
-        return gcdB;
-    }else {
-        return 0;
+    const isGCDANotGood = arrayB.some((value)=> value % gcdA ===0); 
+    const isGCDBNotGood = arrayA.some((value)=> value % gcdB ===0); 
+    let candidate = [0];
+    if(!isGCDANotGood) {
+        candidate.push(gcdA);
+    }
+    if(!isGCDBNotGood) {
+        candidate.push(gcdB);
     }
     
-    function gcd (a,b) {
-        if(b === 0) return a;
-        return gcd(b,a % b);
-    }
+    answer = Math.max(...candidate);
     
+    
+    /**
+     영희가 [5, 17]이 적힌 카드를 갖는다면, 철수가 가진 카드들의 숫자는 모두 10으로 나눌 수 있고, 영희가 가진 카드들의 숫자는 모두 10으로 나눌 수 없습니다. 따라서 철수와 영희는 각각 [10, 20]이 적힌 카드, [5, 17]이 적힌 카드로 나눠 가졌다면 조건에 해당하는 양의 정수 a는 10이 됩니다.
+    */
     return answer;
+}
+
+function gcd(a,b) {
+    if(b === 0) return a;
+    return gcd(b,a%b);
 }
