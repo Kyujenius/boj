0e4dbc1ce12ddfdad3d90fb10c7c18be42bac156
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-19 21:48:10 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 36.46 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,39 +1,36 @@
-function solution(g) {
+function solution(gems) {
     var answer = [];
-    const gems = [...g];
-    const goal =  new Set(gems).size;
+    const gemSet = new Set();
+    gems.forEach((gem)=>gemSet.add(gem));
+    const goal = gemSet.size;
     const gemMap = new Map();
     let leftPtr = 0;
     let rightPtr = 0;
-    let pointerLength = Infinity;
-    gemMap.set(gems[leftPtr],(gemMap.get(gems[leftPtr]) || 0)+1);
-    
-    while(true) {
-        // console.log(gemMap);
-      
-        
-        while(gemMap.size ===goal) {
-            // console.log("들어와서", [leftPtr, rightPtr], gemMap);
-            const size = rightPtr - (leftPtr-1);
-            if(pointerLength > size) {
-                pointerLength = size;
-                answer = [leftPtr+1,rightPtr+1];
+    let length = Infinity;
+    gemMap.set(gems[0], (gemMap.get(gems[0]) || 0) +1);
+    while(rightPtr < gems.length) {
+           while(gemMap.size === goal) {
+            // console.log(`goal 하고 gemMap의 사이즈가 같을 때, leftPtr: ${leftPtr}, rightPtr: ${rightPtr}`);           
+            if(length > (rightPtr - leftPtr)) {
+                answer = [leftPtr+1, rightPtr+1] ;    
+                length = rightPtr-leftPtr;
             }
-            gemMap.set(gems[leftPtr] , gemMap.get(gems[leftPtr]) -1);
             
-            if(gemMap.get(gems[leftPtr]) ===0) {
+            
+            if(gemMap.get(gems[leftPtr]) === 1){
                 gemMap.delete(gems[leftPtr]);
-            }
+            }else {
+                gemMap.set(gems[leftPtr], gemMap.get(gems[leftPtr]) -1);    
+            }    
             leftPtr++;
-            
-
         }
         rightPtr++;
-        if(rightPtr >= gems.length) break;
+        gemMap.set(gems[rightPtr], (gemMap.get(gems[rightPtr]) || 0) +1);
+//         console.log(`leftPtr: ${leftPtr}, rightPtr: ${rightPtr}`);
+//         console.log(gemMap);
+     
         
-        gemMap.set(gems[rightPtr], (gemMap.get(gems[rightPtr])|| 0) +1);
         
     }
-    
     return answer;
 }
