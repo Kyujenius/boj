dcdc043f36da4d77d33b4b0266a2e4b890049b64
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-19 23:43:33 +0900

    [level 2] Title: 리코쳇 로봇, Time: 34.45 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -1,48 +1,50 @@
 function solution(board) {
-    var answer = 0;
-    // console.log(board);
-    
-    
-    const goal = findPosition("G");
-    const start = findPosition("R")
-    
+    var answer = -1;
+    const rPos = findChar(board,"R");
+    const gPos = findChar(board,"G");
+    const visited = Array.from({length: board.length}, ()=> Array(board[0].length).fill(false));
+    // console.log(rPos,gPos);
+    const dx = [-1,1,0,0];
+    const dy = [0,0,-1,1];
     const queue = [];
-    const visited = Array.from({length: board.length},()=> Array(board[0].length).fill(false));
-    queue.push([...start, 0]);
-    // console.log(queue);
-    while(queue.length > 0) {
+    queue.push([...rPos,0])
+    while(queue.length >0) {
         const [x,y,count] = queue.shift();
+        if(x === gPos[0] && y === gPos[1]) {
+            return count;
+        }
         
-        if(x === goal[0] && y === goal[1]) return count;
         
-        const dx = [-1,1,0,0];
-        const dy = [0,0,1,-1];
-        
-        for(let i=  0 ; i<4; i++) {
-            let nx = x + dx[i];
-            let ny = y + dy[i];
-            while((nx>=0 && nx <board[0].length) &&
-                 (ny>=0 && ny<board.length) &&
-                 board[ny][nx] !== 'D') {
+        for(let i = 0 ; i<4; i++) {
+            //한 방향으로 이동하되, D를 만나거나, 벽을 만날 때까지 이동    
+            let nx = dx[i] + x;
+            let ny = dy[i] + y;
+            while((nx >= 0 && nx <board[0].length) &&
+                 (ny >= 0 && ny <board.length) &&
+                 (board[ny][nx] !== "D")) {
                 nx += dx[i];
                 ny += dy[i];
             }
-            nx-=dx[i];
-            ny-=dy[i];
+            nx -= dx[i];
+            ny -= dy[i];
             if(!visited[ny][nx]) {
-                queue.push([nx,ny,count+1]);    
+                queue.push([nx,ny,count+1]);
                 visited[ny][nx] = true;
             }
+            
         }
     }
     
-    function findPosition (char) {
-        for(let i = 0 ; i<board.length; i++) {
-            for(let j = 0 ; j<board[0].length; j++) {
-                if(board[i][j] === char) return [j,i];
+    
+    return answer;
+}
+
+function findChar(board,char) {
+    for(let i = 0; i<board.length; i++) {
+        for(let j = 0; j<board[0].length; j++) {
+            if(char === board[i][j]) {
+                return [j,i];
             }
         }
     }
-    return -1;
 }
-
