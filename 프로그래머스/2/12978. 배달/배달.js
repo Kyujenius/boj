e9ed17271213de7a8a3add0f6a13510dad24acd9
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-19 19:59:13 +0900

    [level 2] Title: 배달, Time: 7.93 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,36 +1,40 @@
 function solution(N, road, K) {
     var answer = 0;
     const dist = Array(N+1).fill(Infinity);
-
-    dist[1]= 0;
-
-    const map = Array.from({length:N+1}, () => []);
-    road.forEach(([from,to,distance])=> {
-        map[from].push({to:to,distance:distance});
-        map[to].push({to:from,distance:distance});
+    const map = {};
+    road.forEach(([from,to,dist])=> {
+        if(map[from] ==undefined) {
+            map[from] = [[to,dist]];
+        }else {
+            map[from].push([to,dist]);
+        }
+        if(map[to] ==undefined) {
+            map[to] = [[from,dist]];
+        }else {
+            map[to].push([from,dist]);
+        }
     })
-    const queue = [{to:1,distance:0}];
+    
+    
+    //최초에는 1로 가는데 0 만큼 쓰는 것으로 시작
+    const queue = [[1,0]];
+    dist[1] = 0;
+    
     while(queue.length > 0) {
-        queue.sort((a,b)=> a.distance - b.distance);
-        const currentObj = queue.shift();
-        const current = currentObj.to;
-        const currentDistance = currentObj.distance;
-        
-        const nextArr = map[current];
-        nextArr.forEach((nextObj)=> {
-            const next = nextObj.to; // 갈 곳
-            const nextDistance = nextObj.distance; // 가는 길에 필요한 거리
-            const nextCost = currentDistance + nextDistance;
-            if(dist[next] > nextCost) {
-                dist[next] = nextCost;
-                queue.push({to:next, distance: nextCost});
+        const [from, fromDist] = queue.shift();
+        map[from].forEach(([to,toDist])=> {
+            if(dist[to] > toDist + fromDist) {
+                queue.push([to,toDist + fromDist]);
+                dist[to] = toDist + fromDist;
             }
-        });
-        // console.log(dist, queue);
+        })
     }
+    // console.log(dist);
+    // console.log(map);
     
-    return dist.filter((d) => d <= K).length;
-    
+    answer =  dist.filter((value)=> value<=K).length;
     
     return answer;
 }
+
+//각 도로를 건너는 데에 걸리는 시간은 도로별로 다르다는 것 -> 다익스트라 알고리즘
