329c9fbe840666989b60914c85d988a4731f4de2
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-19 23:15:16 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 41.88 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -1,41 +1,41 @@
 function solution(orders, course) {
     var answer = [];
-    function getCombinations (arr,selectNumber) {
-        if(selectNumber ===1) return arr.map((value)=> [value]);
-        const results = [];
-        arr.forEach((fixed,index,origin)=> {
-            const array = origin.slice(index+1);
-            const combinations = getCombinations(array, selectNumber-1);
-            const result = combinations.map((value)=> [fixed,...value]);
-            results.push(...result);
-        })
-        return results
-    }
-    
-    
-    course.forEach((courseNum) => {
-        const bestOrderMap = new Map();
-        //각 courseNum 이 getCombinations 의 숫자로 들어가서, Map 을 채운다.
+    course.forEach((courseCount) => {
+        const map = new Map();
         orders.forEach((order)=> {
-            const splittedOrder = order.split("").sort();
-            const combi = getCombinations(splittedOrder,courseNum);
-            combi.forEach((comb)=> {
-                const inputComb = comb.join('');
-                bestOrderMap.set(inputComb, (bestOrderMap.get(inputComb) || 0) +1);
-            })
-        })
-        let bestMenus = [];
-        let bestMenuNum = 0;
-        for(const [key,value] of bestOrderMap) {
-            if(value > bestMenuNum && value >=2) {
-                bestMenuNum = value;
-                bestMenus = [key]
-            }else if(value === bestMenuNum){ 
-                bestMenus.push(key);
+            if(order.length >= courseCount) {
+                const candidate = getCombination(order.split("").sort(),courseCount);
+                candidate.forEach((comb) => {
+                    map.set(comb.join(""),(map.get(comb.join("")) || 0) +1);
+                })
             }
-        }
+        })
+        const obj = Object.fromEntries(map);
+        const entry = Object.entries(obj);
+        entry.sort((a,b)=> b[1] -a[1]);
+        const unsorted = entry.filter((value) => value[1] === entry[0][1] && value[1] >1).map((val)=>val[0]);
+        // unsorted.sort((a,b)=> a-b);
+        unsorted.sort();
+        // console.log(unsorted);
+        answer.push(...unsorted);
         
-        answer.push(...bestMenus);
     })
     return answer.sort();
+}
+
+function getCombination(arr,selectNumber) {
+    const results = [];
+    if(selectNumber == 1) {
+        return arr.map((value)=> [value]);
+    }
+    
+    arr.forEach((fixed,index,origin)=> {
+        const rest = origin.slice(index+1);
+        const combinations = getCombination(rest,selectNumber-1);
+        const attached = combinations.map((combination) => [fixed,...combination]);
+        results.push(...attached);
+    })
+    
+    return results;
+    
 }
