9d64811dd9b433f532e531a6c0f328e02d3eba81
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-20 23:31:43 +0900

    [level 3] Title: 징검다리 건너기, Time: 45.87 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,35 +1,32 @@
 function solution(stones, k) {
     var answer = 0;
-    
-    let left = 1;
+    let left =1;
     let right = 200000000;
     
     while(left<=right) {
         const mid = Math.floor((left+right)/2);
-        console.log(mid);
-        let count = 0 ;
+        let count = 0;
         for(let i = 0; i<stones.length; i++) {
-            if(stones[i] - mid <= 0) {
+            const stone = stones[i];
+            if(stone <= mid) {
                 count++;
+                if(count === k) {
+                    // console.log("같아서 break");
+                    break;
+                }
             }else {
                 count = 0;
             }
-            //수가 너무 커서, 안되는 거니까 줄여버려
-            if(count == k) {
-                right = mid-1;
-                break;
-            }
+        }
+        if(count === k) {
+            // console.log("answer = mid", mid);
+            right = mid -1;
+            answer = mid;
         }
         if(count < k) {
-            answer = mid;
-            left = mid+1;
+            left = mid + 1;            
         }
+        
     }
-    return answer+1;
+    return answer;
 }
-
-//니니즈 친구들은 무제한
-
-//stones는 길이가 20만 
-
-//각 배열의 원소는 2억 이하
