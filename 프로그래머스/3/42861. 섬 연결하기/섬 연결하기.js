6be7ce6ba20054d4a5defa56e2496c3087a3ac00
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-23 21:32:38 +0900

    [level 3] Title: 섬 연결하기, Time: 37.81 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -2,38 +2,47 @@ function solution(n, costs) {
     var answer = 0;
     const map = {};
     costs.forEach(([from,to,cost])=> {
-        if(map[from] === undefined) {
-            map[from] = [{to: to, cost:cost}];            
-        }else{
-            map[from].push({to: to, cost:cost});            
+        if(map[from]===undefined) {
+            map[from] = [{to : to ,cost : cost}];    
+        }else {
+            map[from].push({to : to ,cost : cost});    
         }
         
         if(map[to] === undefined) {
-            map[to] = [{to:from, cost: cost}];
+            map[to] = [{to : from ,cost : cost}];    
         }else {
-            map[to].push({to:from,cost:cost});
+            map[to].push({to : from ,cost : cost});    
         }
-    });
-    
-    const queue= [{to: 0, cost: 0}];
-    const visited = Array(Object.keys(map).length).fill(false);
-    // console.log(visited);
-    while(queue.length > 0) {
-        queue.sort((a,b)=>a.cost - b.cost);
-        // console.log(queue);
-        
-        const {to: from, cost: currentCost} = queue.shift();
-        if(visited[from]) continue;
-        visited[from] = true;
-        answer+= currentCost
-        map[from].forEach(({to:to,cost:toCost})=> {
-            if(!visited[to]) {
-                queue.push({to:to,cost:toCost});
-            }
-        })
-    }
-    
+    })
     // console.log(map);
-    
+    const queue = [{to:0,cost:0}];;
+    const visited = Array(Object.keys(map).length).fill(false);    
+    while(queue.length >0) {
+        console.log(queue);
+        queue.sort((a,b)=>a.cost-b.cost);
+        const {to:from,cost:cost} = queue.shift();
+        if(!visited[from]) {
+            answer +=cost;
+            visited[from] = true;   
+            
+            const sortedToArr = map[from].sort((a,b)=>a.cost-b.cost);
+        
+            sortedToArr.forEach(({to:to, cost:toCost})=> {
+                if(!visited[to]){
+                    queue.push({to:to,cost:toCost});
+                }
+
+            })    
+        }
+        
+
+        // console.log(visited);
+    }
+
     return answer;
 }
+
+
+//최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때 필요한 최소 비용을 return 하도록 solution을 완성
+//100 과 100C2 만큼 costs의 길이
+
