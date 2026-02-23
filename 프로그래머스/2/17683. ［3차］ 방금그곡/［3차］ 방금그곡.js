fb8074e8190a44c833d1e828bd255a35c56838b1
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-23 21:04:00 +0900

    [level 2] Title: [3차] 방금그곡, Time: 8.34 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -1,44 +1,73 @@
 function solution(m, musicinfos) {
-    let bestSongRunTime = 0;
-    let bestSong = '(None)';
-    function transformer(a) {
-        a = a.replace(/C#/g,'c')
-        .replace(/D#/g,'d')
-        .replace(/F#/g,'f')
-        .replace(/G#/g,'g')
-        .replace(/A#/g,'a')
-        .replace(/B#/g,'b');
-        
-        return a;
-    }
-    m = transformer(m);
+    //조건이 일치하는 음악이 없을 때에는 “(None)”을 반환한다.
+    var answer = '(None)';
     
-    musicinfos.forEach((music)=> {
-        let [startTime,endTime,musicName,sound] = music.split(",");
-        const [startH,startM]= startTime.split(":").map(Number);
-        const [endH,endM]= endTime.split(":").map(Number);
-        const runTime = (endH * 60 + endM) - (startH * 60 + startM);
+    const candidate = [];
+    musicinfos.forEach((value, index)=> {
+        const [startTime,endTime,songName,madi] =  value.split(",");
+        const [startHour,startMinute] = startTime.split(":").map(Number);
+        const [endHour,endMinute] = endTime.split(":").map(Number);
         
-        sound = transformer(sound);
+        const start = startHour * 60 + startMinute;
+        const end = endHour * 60 + endMinute;
         
-        let song = "";
+        const playTime = end - start;
+        // console.log(`madi : ${madi}, playTime : ${playTime}`);
+        //madi 변환 
+        const replacedMadi = replace(madi);
         
-        for(let i = 0 ; i<runTime; i++) {
-            song += sound[i % sound.length];
+        //변환한 madi 다시 쪼개기
+        const splittedMadi = replacedMadi.split("");
+        
+        //쪼갠 마디 playTime 동안 넣을 배열
+        const listenedSong = [];
+        
+        for(let i = 0 ; i<playTime; i++) {
+            listenedSong.push(splittedMadi[i % splittedMadi.length]);
         }
         
-        if(song.includes(m)) {
-            if(bestSongRunTime < runTime) {
-                bestSongRunTime = runTime;
-                bestSong = musicName;
-                console.log(bestSong);
-                console.log(song);
-                console.log(m);
-            }
-        }
+        const finalSong = listenedSong.join("");
+        const replacedM = replace(m);
 
+        // console.log(`replacedSong: ${replacedSong}, replacedM: ${replacedM}`);
+        // console.log('---------------');
+        if(finalSong.includes(replacedM)) candidate.push({songName, playTime, index});
 
-        
     })
-    return bestSong;
+    // console.log(candidate);
+    // 조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환한다.
+    // 재생된 시간도 같을 경우 먼저 입력된 음악 제목을 반환한다.
+
+    // 가장 길이가 
+    if(candidate.length === 1){
+        answer = candidate[0].songName;
+    }else if(candidate.length >= 2) {
+        candidate.sort((a,b)=> {
+            return b.playTime - a.playTime; 
+        })
+        
+        const filteredCandidate = candidate.filter((value)=> value.playTime === candidate[0].playTime);
+        filteredCandidate.sort((a,b)=> {
+            return a.index - b.index;
+        })
+        
+        answer = filteredCandidate[0].songName;
+    }else if(candidate.length ===0) {
+        return "(None)";
+    }
+    return answer;
 }
+
+function replace(str) {
+    return str.replace(/C#/g,'c')
+            .replace(/D#/g,'d')
+            .replace(/E#/g,'e')
+            .replace(/F#/g,'f')
+            .replace(/G#/g,'g')
+            .replace(/A#/g,'a')
+            .replace(/B#/g,'b')
+    }
+
+
+
+
