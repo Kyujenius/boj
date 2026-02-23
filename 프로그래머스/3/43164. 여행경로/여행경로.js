3cc7ff1d6687cbef1bbe4870ebae55d5146923c5
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-23 22:23:22 +0900

    [level 3] Title: 여행경로, Time: 3.55 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -3,44 +3,58 @@ function solution(tickets) {
     const map = {};
     
     tickets.forEach(([from,to])=> {
-        if(map[from] === undefined) {
-            map[from] = [to];
+        if(map[from]===undefined) {
+            map[from] = [to]
         }else {
             map[from].push(to);
+            map[from].sort();
         }
     })
-    for(const key in map) {
-        map[key].sort();
-    }
-    
     console.log(map);
     
-    function dfs(from,acc) {
-        if(acc.length === tickets.length+1){
-            answer = acc;
+    const arr = ["ICN"];
+
+    function dfs(from,count) {
+        // console.log(`from:${from},count:${count},arr:${arr}`)
+        if(count === tickets.length) {
+            answer = [...arr];
             return;
         }
         
-        const destination = map[from];
+        if(!map[from]) return;
+        //만약에 티켓이 남아있는데, 갈 곳이 없으면 빠구
+        if(map[from].length === 0) return;
+        
+        const destinations = map[from];
+        
+        for(let i = 0 ; i<destinations.length; i++) {
+            
+            const destination = destinations.splice(i,1);
+            const to = destination[0];
+            arr.push(to);
+            
+            dfs(to, count+1);     
+            
+            if(answer.length>0) return;
+            
+            arr.pop();
+            
+            destinations.splice(i,0,to);
+
+        }
 
         
-        if(!destination) return;
-        if(answer.length>0) return;
-        
-        for(let i = 0; i<destination.length; i++) {
-            const next = destination[i];
-            //하나 잘라두기
-            destination.splice(i,1);
-            
-            dfs(next,[...acc,next]);
-            
-            //복구하기
-            destination.splice(i,0,next);   
-        }
-        
-        
     }
-    dfs("ICN",["ICN"]);
+    dfs("ICN",0);
     
     return answer;
 }
+
+// 항상 "ICN" 공항에서 출발
+
+// 주어진 항공권을 모두 이용해야한다.
+
+// 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다.
+
+// 모든 도시를 방문할 수 없는 경우는 주어지지 않습니다.
+
