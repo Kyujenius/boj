1885c68c03d4b4cd3bcac409a6d131564eadb6c6
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-02-26 23:24:22 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 24.71 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -1,11 +1,10 @@
 function solution(board)
 {
-    var answer = 1234;
     const n = board.length;
     const m = board[0].length;
-    const map = Array.from({length:n},()=> Array(m).fill(0));
+    let maxNumber = 0;
     
-    // 만약 행이나 열이 1개라면, 내부 루프를 돌지 않으므로 직접 확인
+    // 만약 행이나 열이 1개라면 루프를 타지 않으므로 미리 체크
     if (n < 2 || m < 2) {
         for (let i = 0; i < n; i++) {
             for (let j = 0; j < m; j++) {
@@ -15,24 +14,18 @@ function solution(board)
         return 0;
     }
     
-    let maxNum = 0;
-    for(let i = n-2; i>=0 ; i--) {
-        for(let j = m-2; j>= 0; j--) {
-            // console.log(i,j,board);
-            if(board[i][j] === 1) {
-                board[i][j] = check(i,j);
-                maxNum = Math.max(maxNum,board[i][j]);        
+    //i : 횟수 - 돌아봤자 maxCount바퀴가 최대임
+    for(let k = 1; k<n; k++) {
+        for(let j = 1 ; j<m; j++) {
+            if(board[k][j] === 1) {
+                const temp = Math.min(board[k-1][j-1],board[k-1][j],board[k][j-1]) +1;
+                board[k][j] = temp;
+                maxNumber = Math.max(maxNumber,temp);    
             }
+            
             
         }
     }
-    
     // console.log(board);
-    
-    function check(i,j) {
-        return Math.min(board[i+1][j], board[i][j+1], board[i+1][j+1]) + 1;
-    }
-
-    answer = maxNum * maxNum;
-    return answer;
+    return maxNumber*maxNumber;
 }
