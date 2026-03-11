bc0f8e24293eab468027bd953813febd23715a7d
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-03-11 22:37:25 +0900

    [level 2] Title: 요격 시스템, Time: 455.24 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,13 +1,17 @@
 function solution(targets) {
     var answer = 0;
     targets.sort((a,b)=>a[1]-b[1]);
-    console.log(targets);
-    let lastIdx = -1;
-    targets.forEach(([start,end])=> {
-        if(start > lastIdx) {
+    // console.log(targets);
+    let lastTarget = -1;
+    for(let i=  0 ; i<targets.length; i++) {
+        const [start,end] = targets[i]
+        //타겟이 
+        if(lastTarget > start && lastTarget <end) {
+            continue;
+        }else {
+            lastTarget = end - 0.5;
             answer++;
-            lastIdx = end-1
         }
-    })
+    }
     return answer;
 }
