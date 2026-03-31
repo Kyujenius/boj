be0276f040e42193127a6b8dc41036a80e15a248
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-03-31 23:47:04 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 173.79 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -1,50 +1,67 @@
 function solution(diffs, times, limit) {
-    let left = 1;
-    let right = 100000;
+    var answer = 0;
     
-    //30만이라 n^2은 하면 안 됨. -> 이분탐색이다 이거
+    let left = 1; 
+    let right = 100000000;
+    
     while(left <= right) {
-        const level = Math.floor((left + right) / 2);
-        // console.log(level);
-        let myLimit = times[0];
-        //1부터 해버려 걍
-        for(let i=1; i<diffs.length; i++) {
-            const diff = diffs[i];
-            const beforeTime = times[i-1];
-            const time = times[i];
-            if(level >= diff) {
-                // console.log(`level ${level} > diff${diff} - time${time} 더하기`)
-                myLimit += time;
-            }else {
-                const realTime = diff - level;
-                myLimit += (((time + beforeTime) * realTime) + time)
-            }
-            
-            //한 차례 더하고 limit 를 넘기면 mid를 줄여야지
-            if(myLimit > limit) {
-                left = level +1;
-                break;
-            }
-        }
+        let mid = Math.floor((left+right)/2);
         
-        if(myLimit <= limit) right = level -1;        
+        //가능해?
+        if(makeResult(mid)) {
+            // console.log(`mid:${mid}`);
+            answer = mid;
+            right = mid-1; 
+        }
+        //불가능해?
+        else{
+            left = mid+1;
+        }
     }
     
-    return left;
+    // 이분탐색 함수는 더 상위에서 그냥 while문 사용 
+    
+
+    //현재 퍼즐의 난이도를 diff, 현재 퍼즐의 소요 시간을 time_cur, 
+    //이전 퍼즐의 소요 시간을 time_prev, 당신의 숙련도는 level
+
+    // diff ≤ level이면 퍼즐을 틀리지 않고 time_cur만큼의 시간을 사용하여 해결합니다.
+    // diff > level이면, 퍼즐을 총 diff - level번 틀리고,
+    // 퍼즐을 틀릴 때마다, time_cur만큼의 시간을 사용, 추가로 time_prev만큼의 시간을 사용해 이전 퍼즐을 다시 풀고 와야함.
+
+    // 이전 퍼즐을 다시 풀 때는, 이전 퍼즐의 난이도에 상관없이 틀리지 않습니다.
+    // diff - level번 틀린 이후에, 다시 퍼즐을 풀면 time_cur만큼의 시간을 사용하여 퍼즐을 해결합니다.
+    
+    function makeResult(level) {
+        let result = 0;
+        let time_prev = 0;
+        diffs.forEach((diff,index)=> {
+            const time_cur = times[index];
+            if(diff <= level) {
+                result += time_cur;    
+            }else {
+                const wrongTimes = diff-level;
+                result += (wrongTimes * (time_prev + time_cur) + time_cur);
+            }
+            // console.log(result);
+            time_prev = time_cur;
+        })
+        if(result <= limit) {
+            // console.log(`result: ${result}, limit:${limit}`);
+            return true;
+        }else {
+            return false;
+        }
+    }
+    
+    
+    
+    
+    return answer;
 }
 
 
-/**
-*
-현재 퍼즐의 난이도를 diff, 현재 퍼즐의 소요 시간을 time_cur, 
-이전 퍼즐의 소요 시간을 time_prev, 당신의 숙련도를 level이라 하면, 게임은 다음과 같이 진행됩니다.
+//1 ≤ diffs의 길이 = times의 길이 = n ≤ 300,000
 
-//level = 1이면, 퍼즐을 3 - 1 = 2번 틀립니다. 한 번 틀릴 때마다 2 + 4 = 6
+// 시뮬레이션 + 이분 탐색이구나! 
 
-diff ≤ level이면 퍼즐을 틀리지 않고 time_cur만큼의 시간을 사용하여 해결합니다.
-diff > level이면, 퍼즐을 총 diff - level번 틀립니다. 퍼즐을 틀릴 때마다, 
-time_cur만큼의 시간을 사용하며, 추가로 time_prev만큼의 시간을 사용해 이전 퍼즐을 다시 풀고 와야 합니다.
-이전 퍼즐을 다시 풀 때는 이전 퍼즐의 난이도에 상관없이 틀리지 않습니다. 
-diff - level번 틀린 이후에 다시 퍼즐을 풀면 time_cur만큼의 시간을 사용하여 퍼즐을 해결합니다.
-*
-*/
