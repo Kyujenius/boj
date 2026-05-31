41ef1e96eaf2bff229aeff941fbb87b7c0e7321d
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2026-05-31 19:42:28 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 91.55 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -1,67 +1,33 @@
 function solution(diffs, times, limit) {
     var answer = 0;
-    
     let left = 1; 
-    let right = 100000000;
-    
+    let right = 300000;
     while(left <= right) {
-        let mid = Math.floor((left+right)/2);
-        
-        //가능해?
-        if(makeResult(mid)) {
-            // console.log(`mid:${mid}`);
-            answer = mid;
-            right = mid-1; 
-        }
-        //불가능해?
-        else{
-            left = mid+1;
-        }
-    }
-    
-    // 이분탐색 함수는 더 상위에서 그냥 while문 사용 
-    
-
-    //현재 퍼즐의 난이도를 diff, 현재 퍼즐의 소요 시간을 time_cur, 
-    //이전 퍼즐의 소요 시간을 time_prev, 당신의 숙련도는 level
-
-    // diff ≤ level이면 퍼즐을 틀리지 않고 time_cur만큼의 시간을 사용하여 해결합니다.
-    // diff > level이면, 퍼즐을 총 diff - level번 틀리고,
-    // 퍼즐을 틀릴 때마다, time_cur만큼의 시간을 사용, 추가로 time_prev만큼의 시간을 사용해 이전 퍼즐을 다시 풀고 와야함.
-
-    // 이전 퍼즐을 다시 풀 때는, 이전 퍼즐의 난이도에 상관없이 틀리지 않습니다.
-    // diff - level번 틀린 이후에, 다시 퍼즐을 풀면 time_cur만큼의 시간을 사용하여 퍼즐을 해결합니다.
-    
-    function makeResult(level) {
-        let result = 0;
-        let time_prev = 0;
+        // console.log(left,right);
+        let level = Math.floor((left+right)/2);
+        let count = 0;
         diffs.forEach((diff,index)=> {
-            const time_cur = times[index];
-            if(diff <= level) {
-                result += time_cur;    
+            if(level >= diff) {
+                count += times[index];
             }else {
-                const wrongTimes = diff-level;
-                result += (wrongTimes * (time_prev + time_cur) + time_cur);
+                count += (diff-level) * (times[index] + times[index-1]) + times[index];
             }
-            // console.log(result);
-            time_prev = time_cur;
         })
-        if(result <= limit) {
-            // console.log(`result: ${result}, limit:${limit}`);
-            return true;
+        if(count <= limit) {
+            answer = level;
+            right = level-1;
         }else {
-            return false;
+            left = level+1;
         }
     }
-    
-    
-    
-    
     return answer;
 }
 
+//diff -level 번 틀리는데, 그 때마다 time_cur 만큼의 시간을 쓰고, 추가로 time_prev 만큼의 시간을 써서 이전 퍼즐을 풀고 와야함. 
 
-//1 ≤ diffs의 길이 = times의 길이 = n ≤ 300,000
+// 즉 (diff-level) * (time_cur + time_prev) + time_cur
 
-// 시뮬레이션 + 이분 탐색이구나! 
+//제한 시간 내에 퍼즐을 모두 해결하기 위한 숙련도의 최솟값을 구하려고 합니다. 난이도, 소요 시간은 모두 양의 정수며, 숙련도도 양의 정수여야 합니다.
 
+
+// 아무래도 이분 탐색이어야 할 거 같음. 그러나 수를 언제까지로 놔야할지가 조금 헷갈리는 상황
